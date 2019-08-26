35cc0eed48556017383fb403a3162edf94b9c088
Aniq <muhammadaniqizzuddin@gmail.com>
2019-08-26 11:13:41 +0800

    Changed margin of the word in between of words in drawer

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -17,48 +17,48 @@ const SideBar = (props) => {
             style={{ position: 'absolute', top: 0, bottom: 0, right: 0, left: 0 }}>
             <View style={{ padding: 10 }}>
 
-                <TouchableOpacity onPress={() => props.nav('Dashboard')} style={{ flexDirection: 'row', marginTop: 5 }}>
-                    <Ionicons name="logo-buffer" color={'#fff'} style={{ fontSize: 30, paddingRight: 10 }} />
+                <TouchableOpacity onPress={() => props.nav('Dashboard')} style={{ flexDirection: 'row', marginTop: 5, marginBottom: 15  }}>
+                    <Ionicons name="logo-buffer" color={'#fff'} style={{ fontSize: 30, paddingRight: 10, marginBottom: 15 }} />
                     <Text style={[styles.text, { color: '#fff' }]}>Dashboard</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity onPress={() => props.nav('Account')} style={{ flexDirection: 'row', marginTop: 5 }}>
-                    <Ionicons name="ios-briefcase" color={'#fff'} style={{ fontSize: 30, paddingRight: 10 }} />
+                <TouchableOpacity onPress={() => props.nav('Account')} style={{ flexDirection: 'row', marginTop: 5, marginBottom: 15 }}>
+                    <Ionicons name="ios-briefcase" color={'#fff'} style={{ fontSize: 30, paddingRight: 10, marginBottom: 15 }} />
                     <Text style={[styles.text, { color: '#fff' }]}>Account</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity onPress={() => props.nav('LoanApplication')} style={{ flexDirection: 'row', marginTop: 5 }}>
-                    <Ionicons name="ios-create" color={'#fff'} style={{ fontSize: 30, paddingRight: 10 }} />
+                <TouchableOpacity onPress={() => props.nav('LoanApplication')} style={{ flexDirection: 'row', marginTop: 5, marginBottom: 15 }}>
+                    <Ionicons name="ios-create" color={'#fff'} style={{ fontSize: 30, paddingRight: 10, marginBottom: 15 }} />
                     <Text style={[styles.text, { color: '#fff' }]}>Application</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity onPress={() => props.nav('Settings')} style={{ flexDirection: 'row', marginTop: 5 }}>
-                    <Ionicons name="ios-pricetags" color={'#fff'} style={{ fontSize: 30, paddingRight: 10 }} />
+                <TouchableOpacity onPress={() => props.nav('Settings')} style={{ flexDirection: 'row', marginTop: 5, marginBottom: 15 }}>
+                    <Ionicons name="ios-pricetags" color={'#fff'} style={{ fontSize: 30, paddingRight: 10, marginBottom: 15 }} />
                     <Text style={[styles.text, { color: '#fff' }]}>Invoice</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity onPress={() => props.nav('Settings')} style={{ flexDirection: 'row', marginTop: 5 }}>
-                    <Ionicons name="logo-usd" color={'#fff'} style={{ fontSize: 30, paddingRight: 10 }} />
+                <TouchableOpacity onPress={() => props.nav('Settings')} style={{ flexDirection: 'row', marginTop: 5, marginBottom: 15 }}>
+                    <Ionicons name="logo-usd" color={'#fff'} style={{ fontSize: 30, paddingRight: 10, marginBottom: 15 }} />
                     <Text style={[styles.text, { color: '#fff' }]}>Expense</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity onPress={() => props.nav('Settings')} style={{ flexDirection: 'row', marginTop: 5 }}>
-                    <Ionicons name="ios-podium" color={'#fff'} style={{ fontSize: 30, paddingRight: 10 }} />
+                <TouchableOpacity onPress={() => props.nav('Settings')} style={{ flexDirection: 'row', marginTop: 5, marginBottom: 15 }}>
+                    <Ionicons name="ios-podium" color={'#fff'} style={{ fontSize: 30, paddingRight: 10, marginBottom: 15 }} />
                     <Text style={[styles.text, { color: '#fff' }]}>Report</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity onPress={() => props.nav('Settings')} style={{ flexDirection: 'row', marginTop: 5 }}>
-                    <Ionicons name="ios-link" color={'#fff'} style={{ fontSize: 30, paddingRight: 10 }} />
+                <TouchableOpacity onPress={() => props.nav('Settings')} style={{ flexDirection: 'row', marginTop: 5, marginBottom: 15 }}>
+                    <Ionicons name="ios-link" color={'#fff'} style={{ fontSize: 30, paddingRight: 10, marginBottom: 15 }} />
                     <Text style={[styles.text, { color: '#fff' }]}>Business Hub</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity onPress={() => props.nav('Settings')} style={{ flexDirection: 'row', marginTop: 5 }}>
-                    <Ionicons name="ios-help-buoy" color={'#fff'} style={{ fontSize: 30, paddingRight: 10 }} />
+                <TouchableOpacity onPress={() => props.nav('Settings')} style={{ flexDirection: 'row', marginTop: 5, marginBottom: 15 }}>
+                    <Ionicons name="ios-help-buoy" color={'#fff'} style={{ fontSize: 30, paddingRight: 10, marginBottom: 15 }} />
                     <Text style={[styles.text, { color: '#fff' }]}>Support</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity onPress={() => props.nav('Settings')} style={{ flexDirection: 'row', marginTop: 5 }}>
-                    <Ionicons name="ios-settings" color={'#fff'} style={{ fontSize: 30, paddingRight: 10 }} />
+                <TouchableOpacity onPress={() => props.nav('Settings')} style={{ flexDirection: 'row', marginTop: 5, marginBottom: 15 }}>
+                    <Ionicons name="ios-settings" color={'#fff'} style={{ fontSize: 30, paddingRight: 10, marginBottom: 15 }} />
                     <Text style={[styles.text, { color: '#fff' }]}>Settings</Text>
                 </TouchableOpacity>
             </View>
