860c66d3f644fa07caf771c636143ba868a8fabc
aniqizzuddin <muhammadaniqizzuddin@gmail.com>
2019-10-16 13:00:06 +0800

    Edit new invoice page (border for calendar)

--- src/screens/InvoiceScreen.js
+++ src/screens/InvoiceScreen.js
@@ -29,7 +29,7 @@ const InvoiceScreen = (props) => {
                     </TouchableOpacity>
                 </View>
                 <View style={{ flex: 3, justifyContent: 'center', alignItems: 'center' }}>
-                    <Text style={[styles.title, { color: '#055E7C' }]}>Invoices</Text>
+                    <Text style={[styles.title, { color: '#055E7C' }]}>Invoice</Text>
                 </View>
                 <TouchableOpacity onPress={() => props.navigation.navigate('EditProfile')} style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-end', marginRight: 10 }}>
                     <Image source={{ uri: `https://picsum.photos/200/300` }} style={{ width: 30, height: 30, borderRadius: 15 }} />
