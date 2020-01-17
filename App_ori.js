505b0765f18a5237a682b4834b7006dce1222763
Syahrizan Ali Hassan <syahrizan@fintechcapital.co>
2020-01-17 12:41:22 +0800

    change layout on TransferScreen so the bottom buttons no longer hidden on keyboard up

---
+++ App_ori.js
@@ -0,0 +1,237 @@
+import { AppLoading, Notifications } from 'expo';
+import * as Permissions from 'expo-permissions'
+import { Asset } from 'expo-asset';
+import * as Font from 'expo-font';
+import Constants from 'expo-constants'
+import * as SecureStore from 'expo-secure-store'
+import React, { useState, useEffect } from 'react';
+import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { LoggedInContainer, AuthenticationContainer } from './src/navigation/AppNavigator';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import rootReducer from './src/store/reducers/Reducer';
+const store = createStore(rootReducer, applyMiddleware(thunk))
+const App = (props) => {
+  const [isLoadingComplete, setLoadingComplete] = useState(false);
+  const [tokenExists, setTokenExists] = useState(false)
+  const [notification, setNotification] = useState({})
+
+  const checkUpdate = async () => {
+    try {
+      const update = await Expo.Updates.checkForUpdateAsync();
+      if (update.isAvailable) {
+        await Expo.Updates.fetchUpdateAsync();
+        Expo.Updates.reloadFromCache();
+      }
+    } catch (e) {
+      // handle or log error
+    }
+  }
+
+  const checkLogin = async () => {
+    try {
+      //const personalToken = await AsyncStorage.getItem('personalToken');
+      const personalToken = await SecureStore.getItemAsync('personalToken')
+      if (personalToken !== null && !personalToken.includes('error')) {
+        //console.log(`personal token ialah : ${personalToken}`)
+        setTokenExists(true)
+      }
+    } catch (error) {
+      console.log(`personalToken yang aneh error ${JSON.stringify(error)}`)
+      return 'takde'
+    }
+  }
+
+  registerForPushNotificationsAsync = async () => {
+    const { status: existingStatus } = await Permissions.getAsync(
+      Permissions.NOTIFICATIONS
+    );
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      return;
+    }
+    let token = await Notifications.getExpoPushTokenAsync();
+    //console.log(`expo token ialah ${token}`)
+    store.dispatch({ type: 'SET_REGISTER', payload: { expo_token: token } })
+    // console.log(JSON.stringify({
+    //   token: { value: token, }, user: { username: 'Username', },
+    // }))
+  }
+
+  _handleNotification = (notification) => {
+    //console.log(`notification received ${JSON.stringify(notification)}`)
+    const { data } = notification
+    store.dispatch({ type: 'SET_NOTIFICATION_LIST', payload: { ...data } })
+    //store.dispatch({ type: 'SET_NOTIFICATION', payload: { notification } })
+  };
+
+
+  useEffect(() => {
+    //checkUpdate()
+    registerForPushNotificationsAsync();
+    _notificationSubscription = Notifications.addListener(_handleNotification);
+    checkLogin()
+  }, [])
+
+  if (!isLoadingComplete && !props.skipLoadingScreen) {
+    return (
+      <AppLoading
+        startAsync={loadResourcesAsync}
+        onError={handleLoadingError}
+        onFinish={() => handleFinishLoading(setLoadingComplete)}
+      />
+    );
+  } else {
+    return (<Provider store={store}>
+      <View style={styles.container}>
+        <StatusBar barStyle="default" />
+
+        {tokenExists ? <LoggedInContainer /> : <AuthenticationContainer />}
+
+      </View>
+    </Provider>
+    )
+  }
+}
+
+const loadResourcesAsync = async () => {
+  await Promise.all([
+    Asset.loadAsync([
+      require('./src/assets/images/robot-dev.png'),
+      require('./src/assets/images/robot-prod.png'),
+
+      require('./src/assets/images/account.png'),
+      require('./src/assets/images/address.png'),
+      require('./src/assets/images/application.png'),
+      require('./src/assets/images/businesshub.png'),
+      require('./src/assets/images/changeaccount.png'),
+      require('./src/assets/images/changemobilenumber.png'),
+      require('./src/assets/images/changepassword.png'),
+      require('./src/assets/images/city.png'),
+      require('./src/assets/images/company.png'),
+      require('./src/assets/images/compRegNum.png'),
+      require('./src/assets/images/dashboard.png'),
+      require('./src/assets/images/editprofile.png'),
+      require('./src/assets/images/email.png'),
+      require('./src/assets/images/emailicon.png'),
+      require('./src/assets/images/expenses.png'),
+      require('./src/assets/images/icon.png'),
+      require('./src/assets/images/info.png'),
+      require('./src/assets/images/invoice.png'),
+      require('./src/assets/images/logo.png'),
+      require('./src/assets/images/mykad.png'),
+      require('./src/assets/images/notification.png'),
+      require('./src/assets/images/password.png'),
+      require('./src/assets/images/phoneNum.png'),
+      require('./src/assets/images/regDate.png'),
+      require('./src/assets/images/report.png'),
+      require('./src/assets/images/setting.png'),
+      require('./src/assets/images/settingicon.png'),
+      require('./src/assets/images/state.png'),
+      require('./src/assets/images/support.png'),
+      require('./src/assets/images/support1.png'),
+      require('./src/assets/images/position.png'),
+      require('./src/assets/images/user.png'),
+      require('./src/assets/images/logosmall.png'),
+      require('./src/assets/images/logout.png'),
+      require('./src/assets/images/accountNumber.png'),
+      require('./src/assets/images/accountStatus.png'),
+      require('./src/assets/images/accountType.png'),
+      require('./src/assets/images/balanceReport.png'),
+      require('./src/assets/images/cardborder.png'),
+      require('./src/assets/images/contactsuccess.png'),
+      require('./src/assets/images/merchantsuccess.png'),
+      require('./src/assets/images/signupsuccess.png'),
+      require('./src/assets/images/loanapplication.png'),
+      require('./src/assets/images/transfersuccess.png'),
+      require('./src/assets/images/supportsuccess.png'),
+      require('./src/assets/images/invoicesuccess.png'),
+      require('./src/assets/images/documentsubmit.png'),
+      require('./src/assets/images/nameicon.png'),
+      require('./src/assets/images/phonenoicon.png'),
+      require('./src/assets/images/industryicon.png'),
+      require('./src/assets/images/addressicon.png'),
+      require('./src/assets/images/historywhiteicon.png'),
+      require('./src/assets/images/notificationicon.png'),
+      require('./src/assets/images/notificationicon.png'),
+      require('./src/assets/images/itemicon.png'),
+      require('./src/assets/images/customericon.png'),
+      require('./src/assets/images/vendoricon.png'),
+      require('./src/assets/images/changeaccount.png'),
+      require('./src/assets/images/businessdirectory.png'),
+      require('./src/assets/images/zakaticon.png'),
+      require('./src/assets/images/payrollicon.png'),
+      require('./src/assets/images/remittanceicon.png'),
+      require('./src/assets/images/withdrawalsuccess.png'),
+      require('./src/assets/images/itemsuccess.png'),
+      require('./src/assets/images/vendorsuccess.png'),
+      require('./src/assets/images/customersuccess.png'),
+      require('./src/assets/images/passwordsuccess.png'),
+      require('./src/assets/images/emailsuccess.png'),
+      require('./src/assets/images/phonesuccess.png'),
+      require('./src/assets/images/withdrawal.png'),
+      require('./src/assets/images/transfer.png'),
+      require('./src/assets/images/disbursement.png'),
+      require('./src/assets/images/fingerprint.png'),
+      require('./src/assets/images/contactfailed.png'),
+      require('./src/assets/images/invoicefailed.png'),
+      require('./src/assets/images/contactfailed.png'),
+      require('./src/assets/images/itemfailed.png'),
+      require('./src/assets/images/supportfailed.png'),
+      require('./src/assets/images/transferfailed.png'),
+      require('./src/assets/images/withdrawalfailed.png'),
+
+      require('./src/assets/images/bottomTabs/home.png'),
+      require('./src/assets/images/bottomTabs/history.png'),
+      require('./src/assets/images/bottomTabs/notification.png'),
+      require('./src/assets/images/bottomTabs/setting.png'),
+      require('./src/assets/images/bottomTabs/homeBlack.png'),
+      require('./src/assets/images/bottomTabs/historyBlack.png'),
+      require('./src/assets/images/bottomTabs/notificationBlack.png'),
+
+      require('./src/assets/images/screenshots/1.jpeg'),
+      require('./src/assets/images/screenshots/2.jpeg'),
+      require('./src/assets/images/screenshots/3.jpeg'),
+      require('./src/assets/images/screenshots/4.jpeg'),
+      require('./src/assets/images/screenshots/5.jpeg'),
+    ]),
+    Font.loadAsync({
+      // This is the font that we are using for our tab bar
+      ...Ionicons.font,
+      // We include SpaceMono because we use it in HomeScreen.js. Feel free to
+      // remove this if you are not using it in your app
+      'Montserrat_medium': require('./src/assets/fonts/Montserrat/Montserrat-Medium.ttf'),
+      'Montserrat_regular': require('./src/assets/fonts/Montserrat/Montserrat-Regular.ttf'),
+      'Montserrat_light': require('./src/assets/fonts/Montserrat/Montserrat-Light.ttf'),
+      'Montserrat_thin': require('./src/assets/fonts/Montserrat/Montserrat-Thin.ttf'),
+      'Montserrat_bold': require('./src/assets/fonts/Montserrat/Montserrat-Bold.ttf'),
+      'Roboto_medium': require('./src/assets/fonts/Roboto/Roboto-Regular.ttf'),
+    }),
+  ]);
+}
+
+const handleLoadingError = (error) => {
+  // In this case, you might want to report the error to your error reporting
+  // service, for example Sentry
+  console.warn(error);
+}
+
+const handleFinishLoading = (setLoadingComplete) => {
+  setLoadingComplete(true);
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Constants.statusBarHeight,
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
+
+export default App;
--- src/screens/SampleScreen.js
+++ src/screens/SampleScreen.js
@@ -13,7 +13,7 @@ import Layout from '../constants/Layout';
 
 import ScanFinger from '../components/ScanFinger'
 
-const TransferScreen = (props) => {
+const SampleScreen = (props) => {
 
     const unlock = () => {
         setLock(false)
@@ -76,7 +76,6 @@ const TransferScreen = (props) => {
             } else {
                 dispatch(actionCreator.submitNewExpense(values))
                 actions.resetForm({ wallet: account_no, references_no: transferRefNo })
-                props.navigation.navigate('TransferSuccess')
             }
         }}
             initialValues={{ wallet: account_no, references_no: transferRefNo }}
@@ -97,93 +96,26 @@ const TransferScreen = (props) => {
 
                 return (
                     <KeyboardAvoidingView behavior="padding" enabled style={{ flex: 1 }}  >
-                        <Modal transparent={true} animationType={'slide'} visible={authRequestVisible} onRequestClose={() => setAuthRequestVisible(!authRequestVisible)} >
-                            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(1,1,1,0.5)' }}>
-                                <View style={{ flex: 3 }} />
-                                <View style={{ flex: 1, backgroundColor: '#fff', justifyContent: 'center', alignItems: 'center' }}>
-                                    {(authType === 'passcode') ? <CodePin
-                                        //code="2018" // code.length is used if you not pass number prop
-                                        checkPinCode={(code, check) => check(checkCode(code))}
-                                        success={() => unlock()} // If user fill '2018', success is called
-                                        text="Please Enter PIN" // My title
-                                        error="Try again" // If user fail (fill '2017' for instance)
-                                        autoFocusFirst={true} // disabling auto-focus
-                                        keyboardType={'numeric'}
-                                        containerStyle={{ width: Layout.window.width, height: Layout.window.height / 4 }}
-                                    /> : <ScanFinger unlock={unlock} />}
-                                </View>
-                            </View>
-                        </Modal>
-                        <Modal animationType={'slide'} visible={userListView} onRequestClose={() => setuserListView(!userListView)} transparent={false}>
-                            <View style={{ flex: 1, }}>
-                                <View style={[styles.titleMargin, { flex: 1, flexDirection: 'row', borderBottomWidth: 1, borderColor: '#9ADAF4' }]}>
-                                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start', marginLeft: 0 }}>
-                                        <TouchableOpacity onPress={() => props.navigation.goBack()} hitslop={{ top: 20, left: 20, bottom: 20, right: 20 }}>
-                                            <Ionicons name="ios-arrow-back" color={'#3EC2D9'} style={{ fontSize: 30, paddingLeft: 20 }} />
-                                        </TouchableOpacity>
-                                    </View>
-                                    <View style={{ flex: 3, justifyContent: 'center', alignItems: 'center' }}>
-                                        <Text style={[styles.title, { color: '#055E7C' }]}>SELECT USERS</Text>
-                                    </View>
-                                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-end', marginRight: 10 }}>
-                                        <View style={{ backgroundColor: 'rgba(62,194,217,0.5)', borderColor: "#3EC2D9", borderWidth: 0, width: 36, height: 36, borderRadius: 18, justifyContent: 'center', alignItems: 'center' }}>
-                                            <Ionicons name="md-person" color={'#fff'} style={{ fontSize: 25 }} />
-                                        </View>
-                                    </View>
-                                </View>
-                                <View style={{ justifyContent: 'space-between', flex: 9 }}>
-                                    <ScrollView style={[styles.screenMargin]}>
-                                        <View style={{ marginTop: 20 }}>
 
-                                            <View style={{ flexDirection: 'row', justifyContent: 'flex-end', paddingRight: 10 }}>
-                                                <View style={{ flexDirection: 'row', alignItems: 'center', marginLeft: 10, flex: 1, borderWidth: 1, borderColor: 'lightgrey', padding: 10, borderRadius: 10 }}>
-                                                    <View>
-                                                        <Ionicons name="ios-search" color={'#055E7C'} style={{ fontSize: 27, paddingRight: 5 }} />
-                                                    </View>
-                                                    <TextInput placeholder='Please Enter Keyword' style={{ flex: 4 }} />
-                                                    <TouchableOpacity onPress={props.navigation.openDrawer} >
-                                                        <Ionicons name="ios-options" color={'#055E7C'} style={{ fontSize: 27, paddingRight: 5 }} />
-                                                    </TouchableOpacity>
-                                                </View>
-                                            </View>
-                                            {userList && <FlatList data={userList} keyExtractor={(item, index) => index.toString()} renderItem={({ item }) =>
-                                                <TouchableOpacity onPress={() => setUser(item.account_no)} style={styles.box}>
-                                                    <View style={{ flexDirection: 'row', marginTop: 5 }}>
-                                                        <View style={{ flex: 1 }}>
-                                                            <Text style={styles.text}>{item.account_no}</Text>
-                                                        </View>
-                                                    </View>
-                                                    <View style={{ flexDirection: 'row', marginTop: 5 }}>
-                                                        <View style={{ flex: 1 }}>
-                                                            <Text style={styles.small}>{item.business_name}</Text>
-                                                        </View>
-                                                    </View>
-                                                </TouchableOpacity>
-                                            } />}
-                                        </View>
-                                    </ScrollView>
-                                </View >
-                            </View >
-                        </Modal>
                         <View style={{ flex: 1 }}>
                             <View style={[styles.titleMargin, { flex: 1, flexDirection: 'row', borderBottomWidth: 1, borderColor: '#9ADAF4' }]}>
                                 <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start' }}>
-                                    <TouchableOpacity onPress={() => props.navigation.goBack()} hitslop={{ top: 20, left: 20, bottom: 20, right: 20 }}>
+                                    <TouchableOpacity onPress={() => { }} hitslop={{ top: 20, left: 20, bottom: 20, right: 20 }}>
                                         <Ionicons name="ios-arrow-back" color={'#3EC2D9'} style={{ fontSize: 30 }} />
                                     </TouchableOpacity>
                                 </View>
                                 <View style={{ flex: 5, justifyContent: 'center', alignItems: 'center' }}>
                                     <Text style={[styles.title]}>TRANSFER</Text>
                                 </View>
-                                <TouchableOpacity onPress={() => props.navigation.navigate('EditProfile')} style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-end' }}>
+                                <TouchableOpacity onPress={() => { }} style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-end' }}>
                                     <View style={{ backgroundColor: 'rgba(62,194,217,0.5)', borderColor: "#3EC2D9", borderWidth: 0, width: 36, height: 36, borderRadius: 18, justifyContent: 'center', alignItems: 'center' }}>
                                         <Ionicons name="md-person" color={'#fff'} style={{ fontSize: 25 }} />
                                     </View>
                                 </TouchableOpacity>
                             </View>
-                            <View style={[ { flex: 9, justifyContent: 'space-between' }]}>
-                                {/* <Text>{account_no&&JSON.stringify(account_no)}</Text> */}
+                            <View style={[{ flex: 9, justifyContent: 'space-between' }]}>
                                 <View style={styles.screenMargin}>
+                                   
                                     <View style={[styles.formElement]}>
                                         <Text style={[styles.titleBox, { marginBottom: 10, marginTop: 10 }]}>Amount ({currency})</Text>
                                         <TextInput value={amount} onChangeText={FormikProps.handleChange('amount')} onBlur={FormikProps.handleBlur('amount')} style={{ borderWidth: 1, borderColor: amountTouched && amountError ? '#d94498' : 'rgba(0,0,0,0.3)', padding: 5 }} placeholder={amountTouched && amountError ? '' : '100.00'} placeholderTextColor={amountTouched && amountError ? 'rgba(255,0,0,0.3)' : 'lightgrey'} keyboardType={'decimal-pad'} />
@@ -203,7 +135,7 @@ const TransferScreen = (props) => {
                                     </View>
                                 </View>
                                 <View style={{ flexDirection: 'row', alignSelf: 'stretch' }}>
-                                    <TouchableOpacity onPress={() => props.navigation.goBack()} style={{ flex: 1, borderColor: '#D3D3D3', borderWidth: 1, paddingTop: 20, paddingBottom: 20 }}>
+                                    <TouchableOpacity onPress={() => props.navigation.goBack()} style={{ flex: 1, borderColor: '#D3D3D3', borderWidth: 1, paddingTop: 20,paddingBottom: 20 }}>
                                         <LinearGradient colors={['#FFF', '#FFF']} style={{ flex: 1, padding: 10, justifyContent: 'center', alignItems: 'center' }}>
                                             <Text style={[styles.butang, { color: '#000000' }]}>Back</Text>
                                         </LinearGradient>
@@ -216,14 +148,13 @@ const TransferScreen = (props) => {
                                     </TouchableOpacity>
                                 </View>
                             </View>
-
                         </View>
                     </KeyboardAvoidingView>)
             }}
         </Formik >
     );
 }
-TransferScreen.navigationOptions = {
+SampleScreen.navigationOptions = {
     header: null,
 };
-export default TransferScreen;
+export default SampleScreen;
