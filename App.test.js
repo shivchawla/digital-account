36ef4df00c4952a0601832615a69354155ebef16
Syahrizan Ali Hassan <syahrizan@fintechcapital.co>
2020-01-21 14:56:04 +0800

    no comment

--- App.test.js
+++ App.test.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import KucingScreen from './src/screens/KucingScreen';
+import LoanScreen from './src/screens/LoanScreen';
 
 import renderer from 'react-test-renderer';
 
-describe('<App />', () => {
-    it('has 1 child', () => {
-        const cacing = 1
-        expect(cacing).toBe(1);
-    });
-    it('comel sangat', () => {
-        const cacing = 1
-        expect(cacing).toBe(1);
-    });
     it('comel sangat', () => {
         const tree = renderer.create(<KucingScreen />).toJSON();
-        expect(tree.children.length).toBe(1);
+        expect(tree.children.length).toBe(2);
     });
-});
+
+    it('comel sangat', () => {
+        const tree = renderer.create(<KucingScreen />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+    it('comel sangat', () => {
+        const tree = renderer.create(<LoanScreen />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
--- src/screens/KucingScreen.js
+++ src/screens/KucingScreen.js
@@ -6,9 +6,21 @@ const KucingScreen = (props) => {
 
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Testing</Text>
+            <Wow />
+        </View>
+    );
+}
+
+
+const Wow = (props) => {
+
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text>Test</Text>
         </View>
     );
 }
+
 
 export default KucingScreen;
