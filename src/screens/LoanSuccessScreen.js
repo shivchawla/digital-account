615f4713d1e2edf561f81a24a7ec539e23663d1c
Hafizh <eva_01@yahoo.com>
2020-01-31 16:24:48 +0800

    LoanSuccessScreen

--- src/screens/LoanSuccessScreen.js
+++ src/screens/LoanSuccessScreen.js
@@ -21,40 +21,52 @@ const LoanSuccessScreen = (props) => {
     return (
 
         <View style={{ flex: 1, paddingTop: Constants.statusBarHeight }}>
-            {status ? <View style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0 }}>
+            {true ? <View style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0 }}>
                 <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                    <View style={{ width: Layout.window.width * 0.85, justifyContent: 'center', alignItems: 'center' }}>
-                        <Image source={require('../assets/images/logo.png')} style={{ height: Layout.window.height * 0.2, width: Layout.window.width * 0.5 }} resizeMode={'contain'} />
-                        <Image source={require('../assets/images/loanapplication.png')} style={{ height: Layout.window.height * 0.3, width: Layout.window.width * 0.7 }} resizeMode={'contain'} />
-                        <Text style={[styles.h3, { margin: 5, fontWeight: 'bold' }]}>Loan Application Submitted</Text>
-                        <View style={{ alignSelf: 'stretch', flexDirection: 'column', margin: 5, alignItems: 'center' }}>
-                            <Text style={[styles.text, { margin: 5, color: 'darkturquoise' }]}>Congratulation!</Text>
-                            <Text style={[styles.text, { margin: 5, marginBottom: 20 }]}>Your application has been submitted. The result will be notified to you in three days time.</Text>
+                <View style={{ flex: 1, justifyContent: 'flex-end' }}>
+                            <Image source={require('../assets/images/logo.png')} style={{ height: Layout.window.height * 0.2, width: Layout.window.width * 0.5 }} resizeMode={'contain'} />
                         </View>
+                        <View style={{ flex: 2, alignSelf: 'stretch' }}>
+                            <Image source={require('../assets/images/loanapplication.png')} style={{ flex: 1, height: undefined, width: undefined }} resizeMode={'contain'} />
+                        </View>
+                        <View style={{ flex: 1, alignItems: 'center' }}>
+                            <Text style={[styles.h3, { margin: 5, fontWeight: 'bold' }]}>Loan Application Submitted</Text>
+                            <View style={{ alignSelf: 'stretch', flexDirection: 'column', margin: 5, alignItems: 'center' }}>
+                                <Text style={[styles.text, { margin: 5, color: 'darkturquoise' }]}>Congratulation!</Text>
+                                <Text style={[styles.text, { margin: 5, marginBottom: 20, textAlign: 'center' }]}>Your application has been submitted. The result will be notified to you in three days time.</Text>
+                            </View>
+                        </View>
+                        <View style = {{flex:1}}>
                         <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
                             <TouchableOpacity onPress={() => goDashboard()} style={{ width: Layout.window.width * 0.4, paddingTop: 5, paddingBottom: 5, borderWidth: 1, borderColor: '#4A90E2', borderRadius: 15, justifyContent: 'center', alignItems: 'center', margin: 10 }}>
                                 <Text style={[styles.textDefault]}>Dashboard</Text>
                             </TouchableOpacity>
                         </View>
-                    </View>
+                        </View>
                 </View>
             </View> : <View style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0 }}>
                     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                        <View style={{ width: Layout.window.width * 0.85, justifyContent: 'center', alignItems: 'center' }}>
+                    <View style={{ flex: 1, justifyContent: 'flex-end' }}>
                             <Image source={require('../assets/images/logo.png')} style={{ height: Layout.window.height * 0.2, width: Layout.window.width * 0.5 }} resizeMode={'contain'} />
-                            <Image source={require('../assets/images/loanfailed.png')} style={{ height: Layout.window.height * 0.3, width: Layout.window.width * 0.7 }} resizeMode={'contain'} />
-                            <Text style={[styles.h3, { margin: 5, fontWeight: 'bold' }]}>Loan Application Failed</Text>
+                        </View>
+                        <View style={{ flex: 2, alignSelf: 'stretch' }}>
+                            <Image source={require('../assets/images/loanfailed.png')} style={{ flex: 1, height: undefined, width: undefined }} resizeMode={'contain'} />
+                        </View>
+                        <View style={{ flex: 1, alignItems: 'center' }}>
+                            <Text style={[styles.h3, { margin: 5, fontWeight: 'bold' }]}>Loan Application Failed!</Text>
                             <View style={{ alignSelf: 'stretch', flexDirection: 'column', margin: 5, alignItems: 'center' }}>
                                 <Text style={[styles.text, { margin: 5, color: 'darkturquoise' }]}>Unfortunately!</Text>
-                                <Text style={[styles.text, { margin: 5, marginBottom: 20 }]}>Please try again later.</Text>
+                                <Text style={[styles.text, { margin: 5, marginBottom: 20, textAlign: 'center' }]}>Please Try Again Later.</Text>
                             </View>
+                        </View>
+                        <View style = {{flex:1}}>
                             <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
                                 <TouchableOpacity onPress={() => goDashboard()} style={{ width: Layout.window.width * 0.4, paddingTop: 5, paddingBottom: 5, borderWidth: 1, borderColor: '#4A90E2', borderRadius: 15, justifyContent: 'center', alignItems: 'center', margin: 10 }}>
                                     <Text style={[styles.textDefault]}>Dashboard</Text>
                                 </TouchableOpacity>
                             </View>
+                            </View>
                         </View>
-                    </View>
                 </View>}
         </View>
 
