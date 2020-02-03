5d27e3a44aeaa7624aa240db3be0a5956aefdd45
Syahrizan Ali Hassan <syahrizan@fintechcapital.co>
2020-02-03 12:18:18 +0800

    image=> icon

--- src/screens/EditProfileScreen.js
+++ src/screens/EditProfileScreen.js
@@ -67,16 +67,18 @@ const EditProfileScreen = (props) => {
                                 <Text style={[styles.title]}>EDIT PROFILE</Text>
                             </View>
                             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-end', marginRight: 10 }}>
-                                <View style={{ backgroundColor:'rgba(62,194,217,0.5)',borderColor: "#3EC2D9", borderWidth: 0, width: 36, height: 36, borderRadius: 18, justifyContent: 'center', alignItems: 'center' }}>
-            <Ionicons name="md-person" color={'#fff'} style={{ fontSize: 25 }} />
-          </View>
+                                <View style={{ backgroundColor: 'rgba(62,194,217,0.5)', borderColor: "#3EC2D9", borderWidth: 0, width: 36, height: 36, borderRadius: 18, justifyContent: 'center', alignItems: 'center' }}>
+                                    <Ionicons name="md-person" color={'#fff'} style={{ fontSize: 25 }} />
+                                </View>
                             </View>
                         </View>
                         <View style={{ justifyContent: 'space-between', flex: 9 }}>
                             <View style={[{ flex: 9 }]}>
                                 <ScrollView style={[styles.screenMargin]}>
                                     <View style={{ flexDirection: 'row', justifyContent: 'flex-start', marginBottom: 10, marginTop: 25, marginBottom: 25 }}>
-                                        <Image source={{ uri: `https://picsum.photos/200/300` }} style={{ width: 90, height: 90, }} />
+                                        <View style={{ backgroundColor: 'rgba(62,194,217,0.5)', borderColor: "#3EC2D9", borderWidth: 1, width: 90, height: 90, justifyContent: 'center', alignItems: 'center' }}>
+                                            <Ionicons name="md-person" color={'#fff'} style={{ fontSize: 60 }} />
+                                        </View>
                                     </View>
                                     <View style={[styles.formElement]}>
                                         <Text style={[styles.titleBox, { marginBottom: 10 }]}>Company Name</Text>
@@ -110,10 +112,10 @@ const EditProfileScreen = (props) => {
                             </View>
                             <View style={{ flex: 1, flexDirection: 'row', alignSelf: 'stretch' }}>
                                 <TouchableOpacity onPress={() => props.navigation.goBack()} style={{ flex: 1, borderColor: '#D3D3D3', borderWidth: 1, paddingTop: 20, paddingBottom: 20, justifyContent: 'center', alignItems: 'center' }}>
-                                        
-                                            <Text style={[styles.butang, { color: '#000000' }]}>Back</Text>
-                                        
-                                    </TouchableOpacity>
+
+                                    <Text style={[styles.butang, { color: '#000000' }]}>Back</Text>
+
+                                </TouchableOpacity>
                                 <TouchableOpacity disabled={!FormikProps.isValid} onPress={FormikProps.handleSubmit} style={{ flex: 1 }}>
                                     <LinearGradient colors={FormikProps.isValid ? ['#0A6496', '#055E7C'] : ['rgba(10,100,150,0.5)', 'rgba(5,94,124,0.5)']} style={{ flex: 1, padding: 10, justifyContent: 'center', alignItems: 'center' }}>
                                         {FormikProps.isSubmitting ? <ActivityIndicator color={'#fff'} /> : <Text style={[styles.butang, { color: '#fff' }]}>Save</Text>}
