59b0b8523a0066febd13b0295be2bbf96685e50e
Hafizh <eva_01@yahoo.com>
2020-02-03 12:42:41 +0800

    Fix keyboardavoidingview

--- src/screens/LoanApplicationDeclarationScreen.js
+++ src/screens/LoanApplicationDeclarationScreen.js
@@ -69,7 +69,7 @@ const LoanApplicationDeclarationScreen = (props) => {
                 }
 
                 return (
-                    <KeyboardAvoidingView behavior="padding" enabled style={{ flex: 1 }}>
+                    <KeyboardAvoidingView behavior="padding" enabled style={{ flex: 1 }} keyboardVerticalOffset={20} >
                         <View style={[{ flex: 1, flexDirection: 'row', borderBottomWidth: 1, borderColor: '#9ADAF4' }]}>
                             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start', marginLeft: 0 }}>
                                 <TouchableOpacity onPress={() => props.navigation.goBack()} hitslop={{ top: 20, left: 20, bottom: 20, right: 20 }}>
@@ -156,7 +156,7 @@ const LoanApplicationDeclarationScreen = (props) => {
                                     </View>
                                 </ScrollView>
                             </View>
-                            <View style={{ flex: 1, flexDirection: 'row', alignSelf: 'stretch' }}>
+                            <View style={{ flexDirection: 'row', alignSelf: 'stretch' }}>
                                 <TouchableOpacity onPress={() => props.navigation.goBack()} style={{ flex: 1, borderColor: '#D3D3D3', borderWidth: 1 }}>
                                     <LinearGradient colors={['#FFF', '#FFF']} style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                                         <Text style={[styles.butang, { color: '#000000' }]}>Back</Text>
