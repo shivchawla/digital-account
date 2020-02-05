a3214d1b6b586be152138c07a53ffb1975c2b434
syahrizan <syahrizan@fcgb.com.my>
2020-02-05 15:10:06 +0800

    IOS picker for Zakat

--- src/screens/ZakatScreen.js
+++ src/screens/ZakatScreen.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { View, TouchableOpacity, Text, Image, ActivityIndicator, KeyboardAvoidingView, TextInput, ScrollView, Picker } from 'react-native';
+import React, { useState } from 'react';
+import { View, TouchableOpacity, Text, Image, ActivityIndicator, KeyboardAvoidingView, TextInput, ScrollView, Picker, Platform,Modal } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient'
 import { Ionicons } from '@expo/vector-icons';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
 import styles from '../styles/styles'
-
+import Constants from 'expo-constants';
 const validationSchema = Yup.object().shape({
 
     typeZakat: Yup
@@ -41,7 +41,17 @@ const validationSchema = Yup.object().shape({
 
 });
 
+
+
 const ZakatScreen = (props) => {
+    const [iosPickerVisible, setIosPickerVisible] = useState(false)
+    const [modalContent, setModalContent] = useState(null)
+    const ios = Platform.OS === "ios" ? true : false
+
+    const handleIosPicker=(modalContent)=>{
+        setModalContent(modalContent)
+        setIosPickerVisible(!iosPickerVisible)
+    }
 
     return (
         <Formik onSubmit={async values => {
@@ -74,6 +84,39 @@ const ZakatScreen = (props) => {
                 return (
 
                     <KeyboardAvoidingView behavior="padding" enabled style={{ flex: 1, }} keyboardVerticalOffset={20}>
+                        <Modal animationType={'slide'}
+                            visible={iosPickerVisible} onRequestClose={() => console.log(`onRequestClose`)}
+                        >
+                            <View style={{ flex: 1, paddingTop: Constants.statusBarHeight }}>
+                                <View style={[styles.titleMargin, { flex: 1, flexDirection: 'row', borderBottomWidth: 1, borderColor: '#9ADAF4' }]}>
+                                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start' }}>
+                                        <TouchableOpacity onPress={() => setIosPickerVisible(!iosPickerVisible)} hitslop={{ top: 20, left: 20, bottom: 20, right: 20 }}>
+                                            <Ionicons name="ios-arrow-back" color={'#3EC2D9'} style={{ fontSize: 30 }} />
+                                        </TouchableOpacity>
+                                    </View>
+                                    <View style={{ flex: 5, justifyContent: 'center', alignItems: 'center' }}>
+                                        <Text style={[styles.title]}>Select</Text>
+                                    </View>
+                                    <View style={{ flex: 1 }} />
+                                </View>
+                                <View style={{ flex: 9, justifyContent: 'flex-start' }}>
+                                    {modalContent === 'typeZakat' ?
+                                        <Picker style={{ flex: 1, height: 35 }} selectedValue={typeZakat} onValueChange={(itemValue, itemIndex) => FormikProps.setFieldValue('typeZakat', itemValue)}>
+                                            <Picker.Item label={'Please Select'} value={undefined} />
+                                            <Picker.Item label="Zakat Pendapatan" value="zakatPendapatan" />
+                                            <Picker.Item label="Zakat Perniagaan" value="zakatPerniagaan" />
+                                            <Picker.Item label="Zakat Simpanan" value="zakatSimpanan" />
+                                        </Picker> : <Picker style={{ flex: 1, height: 35 }} selectedValue={payZakatTo} onValueChange={(itemValue, itemIndex) =>
+                                                FormikProps.setFieldValue('payZakatTo', itemValue)}>
+                                                <Picker.Item label={'Please Select'} value={undefined} />
+                                                <Picker.Item label="Zakat Selangor" value="zakatSelangor" />
+                                                <Picker.Item label="Zakat Perak" value="zakatPerak" />
+                                                <Picker.Item label="Zakat Terengganu" value="zakatTerengganu" />
+                                            </Picker>
+                                    }
+                                </View>
+                            </View>
+                        </Modal>
                         <View style={{ flex: 1, flexDirection: 'row', borderBottomWidth: 1, borderColor: '#9ADAF4', marginBottom: 25 }}>
                             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start', marginLeft: 0 }}>
                                 <TouchableOpacity onPress={() => props.navigation.goBack()} hitslop={{ top: 20, left: 20, bottom: 20, right: 20 }}>
@@ -84,9 +127,9 @@ const ZakatScreen = (props) => {
                                 <Text style={[styles.title]}>Zakat Details</Text>
                             </View>
                             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-end', marginRight: 10 }}>
-                                <View style={{ backgroundColor:'rgba(62,194,217,0.5)',borderColor: "#3EC2D9", borderWidth: 0, width: 36, height: 36, borderRadius: 18, justifyContent: 'center', alignItems: 'center' }}>
-            <Ionicons name="md-person" color={'#fff'} style={{ fontSize: 25 }} />
-          </View>
+                                <View style={{ backgroundColor: 'rgba(62,194,217,0.5)', borderColor: "#3EC2D9", borderWidth: 0, width: 36, height: 36, borderRadius: 18, justifyContent: 'center', alignItems: 'center' }}>
+                                    <Ionicons name="md-person" color={'#fff'} style={{ fontSize: 25 }} />
+                                </View>
                             </View>
                         </View>
                         <View style={{ justifyContent: 'space-between', flex: 9 }}>
@@ -94,19 +137,31 @@ const ZakatScreen = (props) => {
                                 <ScrollView >
                                     <View style={[styles.formElement, { alignSelf: 'stretch' }]}>
                                         <Text style={[styles.titleBox, { marginBottom: 10 }]}>Type of Zakat</Text>
-                                        <View style={{ alignSelf: 'stretch', borderWidth: 1, borderColor: 'rgba(0,0,0,0.3)' }}>
-                                            <Picker style={{ flex: 1, height: 35 }} selectedValue={typeZakat} onValueChange={(itemValue, itemIndex) => FormikProps.setFieldValue('typeZakat', itemValue)}>
-                                                <Picker.Item label={'Please Select'} value={undefined} />
-                                                <Picker.Item label="Zakat Pendapatan" value="zakatPendapatan" />
-                                                <Picker.Item label="Zakat Perniagaan" value="zakatPerniagaan" />
-                                                <Picker.Item label="Zakat Simpanan" value="zakatSimpanan" />
-                                            </Picker>
-                                            {typeZakatTouched && typeZakatError && <Text style={styles.error}>{typeZakatError}</Text>}
-                                        </View>
+                                        {ios ?
+                                            <View style={{ alignSelf: 'stretch', borderWidth: 1, borderColor: 'rgba(0,0,0,0.3)' }}>
+                                                <TouchableOpacity style={{ paddingTop: 10 }} onPress={() => handleIosPicker('typeZakat')}>
+                                                    <Text style={[styles.small, { color: '#0A6496' }]}>{typeZakat ? typeZakat : `Select Type`}</Text>
+                                                </TouchableOpacity>
+                                                {typeZakatTouched && typeZakatError && <Text style={styles.error}>{typeZakatError}</Text>}
+                                            </View> : <View style={{ alignSelf: 'stretch', borderWidth: 1, borderColor: 'rgba(0,0,0,0.3)' }}>
+                                                <Picker style={{ flex: 1, height: 35 }} selectedValue={typeZakat} onValueChange={(itemValue, itemIndex) => FormikProps.setFieldValue('typeZakat', itemValue)}>
+                                                    <Picker.Item label={'Please Select'} value={undefined} />
+                                                    <Picker.Item label="Zakat Pendapatan" value="zakatPendapatan" />
+                                                    <Picker.Item label="Zakat Perniagaan" value="zakatPerniagaan" />
+                                                    <Picker.Item label="Zakat Simpanan" value="zakatSimpanan" />
+                                                </Picker>
+                                                {typeZakatTouched && typeZakatError && <Text style={styles.error}>{typeZakatError}</Text>}
+                                            </View>}
                                     </View>
                                     <View style={[styles.formElement, { alignSelf: 'stretch' }]}>
                                         <Text style={[styles.titleBox, { marginBottom: 10 }]}>Pay Zakat To</Text>
-                                        <View style={{ alignSelf: 'stretch', borderWidth: 1, borderColor: 'rgba(0,0,0,0.3)' }}>
+                                        {ios ?
+                                            <View style={{ alignSelf: 'stretch', borderWidth: 1, borderColor: 'rgba(0,0,0,0.3)' }}>
+                                                <TouchableOpacity style={{ paddingTop: 10 }} onPress={() => handleIosPicker('payZakatTo')}>
+                                                    <Text style={[styles.small, { color: '#0A6496' }]}>{payZakatTo ? payZakatTo : `Zakat To`}</Text>
+                                                </TouchableOpacity>
+                                                {payZakatToTouched && payZakatToError && <Text style={styles.error}>{payZakatToError}</Text>}
+                                            </View> :<View style={{ alignSelf: 'stretch', borderWidth: 1, borderColor: 'rgba(0,0,0,0.3)' }}>
                                             <Picker style={{ flex: 1, height: 35 }} selectedValue={payZakatTo} onValueChange={(itemValue, itemIndex) =>
                                                 FormikProps.setFieldValue('payZakatTo', itemValue)}>
                                                 <Picker.Item label={'Please Select'} value={undefined} />
@@ -115,7 +170,7 @@ const ZakatScreen = (props) => {
                                                 <Picker.Item label="Zakat Terengganu" value="zakatTerengganu" />
                                             </Picker>
                                             {payZakatToTouched && payZakatToError && <Text style={styles.error}>{payZakatToError}</Text>}
-                                        </View>
+                                        </View>}
                                     </View>
                                     <View style={[styles.formElement]}>
                                         <Text style={[styles.titleBox, { marginBottom: 10 }]}>Amount</Text>
@@ -141,10 +196,10 @@ const ZakatScreen = (props) => {
                             </View>
                             <View style={{ flexDirection: 'row', alignSelf: 'stretch' }}>
                                 <TouchableOpacity onPress={() => props.navigation.goBack()} style={{ flex: 1, borderColor: '#D3D3D3', borderWidth: 1, paddingTop: 20, paddingBottom: 20, justifyContent: 'center', alignItems: 'center' }}>
-                                        
-                                            <Text style={[styles.butang, { color: '#000000' }]}>Back</Text>
-                                        
-                                    </TouchableOpacity>
+
+                                    <Text style={[styles.butang, { color: '#000000' }]}>Back</Text>
+
+                                </TouchableOpacity>
                                 <TouchableOpacity disabled={!FormikProps.isValid} onPress={FormikProps.handleSubmit} style={{ flex: 1 }}>
                                     <LinearGradient colors={FormikProps.isValid ? ['#0A6496', '#055E7C'] : ['rgba(10,100,150,0.5)', 'rgba(5,94,124,0.5)']} style={{ flex: 1, padding: 10, justifyContent: 'center', alignItems: 'center' }}>
                                         {FormikProps.isSubmitting ? <ActivityIndicator color={'#fff'} /> : <Text style={[styles.butang, { color: '#fff' }]}>Submit</Text>}
