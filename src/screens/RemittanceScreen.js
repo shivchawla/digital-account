03251eeb8076c6f8d17f08537d39414859023fb1
syahrizan <syahrizan@fcgb.com.my>
2020-02-05 15:22:58 +0800

    IOS picker for Remittance

--- src/screens/RemittanceScreen.js
+++ src/screens/RemittanceScreen.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { View, TouchableOpacity, Text, Image, ActivityIndicator, KeyboardAvoidingView, TextInput, ScrollView, Picker } from 'react-native';
+import React, { useState } from 'react';
+import { View, TouchableOpacity, Text, Image, ActivityIndicator, KeyboardAvoidingView, TextInput, ScrollView, Picker, Modal, Platform } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient'
 import { Ionicons } from '@expo/vector-icons';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
 import styles from '../styles/styles'
+import Constants from 'expo-constants';
 
 const validationSchema = Yup.object().shape({
 
@@ -47,6 +48,14 @@ const validationSchema = Yup.object().shape({
 });
 
 const RemittanceScreen = (props) => {
+    const [iosPickerVisible, setIosPickerVisible] = useState(false)
+    const [modalContent, setModalContent] = useState(null)
+    const ios = Platform.OS === "ios" ? true : false
+
+    const handleIosPicker = (modalContent) => {
+        setModalContent(modalContent)
+        setIosPickerVisible(!iosPickerVisible)
+    }
 
     return (
 
@@ -83,6 +92,31 @@ const RemittanceScreen = (props) => {
                 return (
 
                     <KeyboardAvoidingView behavior="padding" enabled style={{ flex: 1, }} keyboardVerticalOffset={20} >
+                        <Modal animationType={'slide'}
+                            visible={iosPickerVisible} onRequestClose={() => console.log(`onRequestClose`)}
+                        >
+                            <View style={{ flex: 1, paddingTop: Constants.statusBarHeight }}>
+                                <View style={[styles.titleMargin, { flex: 1, flexDirection: 'row', borderBottomWidth: 1, borderColor: '#9ADAF4' }]}>
+                                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start' }}>
+                                        <TouchableOpacity onPress={() => setIosPickerVisible(!iosPickerVisible)} hitslop={{ top: 20, left: 20, bottom: 20, right: 20 }}>
+                                            <Ionicons name="ios-arrow-back" color={'#3EC2D9'} style={{ fontSize: 30 }} />
+                                        </TouchableOpacity>
+                                    </View>
+                                    <View style={{ flex: 5, justifyContent: 'center', alignItems: 'center' }}>
+                                        <Text style={[styles.title]}>Select</Text>
+                                    </View>
+                                    <View style={{ flex: 1 }} />
+                                </View>
+                                <View style={{ flex: 9, justifyContent: 'flex-start' }}>
+                                    <Picker style={{ flex: 1, height: 35, borderColor: '#055E7C', borderWidth: 1 }} selectedValue={modalContent==='recipientCurrency'?recipientCurrency:senderCurrency} onValueChange={(itemValue, itemIndex) => {
+                                        modalContent==='recipientCurrency'?FormikProps.setFieldValue('recipientCurrency', itemValue):FormikProps.setFieldValue('senderCurrency', itemValue)}}>
+                                        <Picker.Item label={'Please Select'} value={undefined} />
+                                        <Picker.Item label="Malaysian Ringgit (MYR)" value="myr" />
+                                        <Picker.Item label="Indonesian Rupiah (IDR)" value="idr" />
+                                    </Picker>
+                                </View>
+                            </View>
+                        </Modal>
                         <View style={{ flex: 1, flexDirection: 'row', borderBottomWidth: 1, borderColor: '#9ADAF4', marginBottom: 25 }}>
                             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start', marginLeft: 0 }}>
                                 <TouchableOpacity onPress={() => props.navigation.goBack()} hitslop={{ top: 20, left: 20, bottom: 20, right: 20 }}>
@@ -103,26 +137,38 @@ const RemittanceScreen = (props) => {
                                 <ScrollView style={styles.screenMargin}>
                                     <View style={[styles.formElement, { alignSelf: 'stretch' }]}>
                                         <Text style={[styles.titleBox], { marginBottom: 10 }}>From</Text>
-                                        <View style={{ alignSelf: 'stretch', borderWidth: 1, borderColor: 'rgba(0,0,0,0.3)', marginBottom: 10 }}>
-                                            <Picker style={{ flex: 1, height: 35, borderColor: '#055E7C', borderWidth: 1 }} selectedValue={senderCurrency} onValueChange={(itemValue, itemIndex) => FormikProps.setFieldValue('senderCurrency', itemValue)}>
-                                                <Picker.Item label={'Please Select'} value={undefined} />
-                                                <Picker.Item label="Malaysian Ringgit (MYR)" value="myr" />
-                                                <Picker.Item label="Indonesian Rupiah (IDR)" value="idr" />
-                                            </Picker>
-                                        </View>
+                                        {ios ?
+                                            <View style={{ alignSelf: 'stretch', borderWidth: 1, borderColor: 'rgba(0,0,0,0.3)' }}>
+                                                <TouchableOpacity style={{ paddingTop: 10 }} onPress={() => handleIosPicker('senderCurrency')}>
+                                                    <Text style={[styles.small, { color: '#0A6496' }]}>{senderCurrency ? senderCurrency : `Sender Currency`}</Text>
+                                                </TouchableOpacity>
+                                                {senderCurrencyError && senderCurrencyTouched && <Text style={styles.error}>{senderCurrencyTouched}</Text>}
+                                            </View> : <View style={{ alignSelf: 'stretch', borderWidth: 1, borderColor: 'rgba(0,0,0,0.3)', marginBottom: 10 }}>
+                                                <Picker style={{ flex: 1, height: 35, borderColor: '#055E7C', borderWidth: 1 }} selectedValue={senderCurrency} onValueChange={(itemValue, itemIndex) => FormikProps.setFieldValue('senderCurrency', itemValue)}>
+                                                    <Picker.Item label={'Please Select'} value={undefined} />
+                                                    <Picker.Item label="Malaysian Ringgit (MYR)" value="myr" />
+                                                    <Picker.Item label="Indonesian Rupiah (IDR)" value="idr" />
+                                                </Picker>
+                                            </View>}
                                         {senderCurrencyError && senderCurrencyTouched && <Text style={styles.error}>{senderCurrencyTouched}</Text>}
                                         <TextInput value={senderAmount} onChangeText={FormikProps.handleChange('senderAmount')} onBlur={FormikProps.handleBlur('senderAmount')} style={{ borderWidth: 1, borderColor: senderAmountTouched && senderAmountError ? '#d94498' : 'rgba(0,0,0,0.3)', padding: 5 }} placeholder={senderAmountTouched && senderAmountError ? '' : 'MYR 90.00'} placeholderTextColor={senderAmountTouched && senderAmountError ? 'rgba(255,0,0,0.3)' : 'lightgrey'} keyboardType={'phone-pad'} />
                                         {senderAmountTouched && senderAmountError && <Text style={styles.error}>{senderAmountError}</Text>}
                                     </View>
                                     <View style={[styles.formElement, { alignSelf: 'stretch' }]}>
                                         <Text style={[styles.titleBox], { marginBottom: 10 }}>To</Text>
-                                        <View style={{ alignSelf: 'stretch', borderWidth: 1, borderColor: 'rgba(0,0,0,0.3)', marginBottom: 10 }}>
-                                            <Picker style={{ flex: 1, height: 35, borderColor: '#055E7C', borderWidth: 1 }} selectedValue={recipientCurrency} onValueChange={(itemValue, itemIndex) => FormikProps.setFieldValue('recipientCurrency', itemValue)}>
-                                                <Picker.Item label={'Please Select'} value={undefined} />
-                                                <Picker.Item label="Malaysian Ringgit (MYR)" value="myr" />
-                                                <Picker.Item label="Indonesian Rupiah (IDR)" value="idr" />
-                                            </Picker>
-                                        </View>
+                                        {ios ?
+                                            <View style={{ alignSelf: 'stretch', borderWidth: 1, borderColor: 'rgba(0,0,0,0.3)' }}>
+                                                <TouchableOpacity style={{ paddingTop: 10 }} onPress={() => handleIosPicker('recipientCurrency')}>
+                                                    <Text style={[styles.small, { color: '#0A6496' }]}>{recipientCurrency ? recipientCurrency : `Recipient Currency`}</Text>
+                                                </TouchableOpacity>
+                                                {recipientCurrencyError && recipientCurrencyTouched && <Text style={styles.error}>{recipientCurrencyTouched}</Text>}
+                                            </View> : <View style={{ alignSelf: 'stretch', borderWidth: 1, borderColor: 'rgba(0,0,0,0.3)', marginBottom: 10 }}>
+                                                <Picker style={{ flex: 1, height: 35, borderColor: '#055E7C', borderWidth: 1 }} selectedValue={recipientCurrency} onValueChange={(itemValue, itemIndex) => FormikProps.setFieldValue('recipientCurrency', itemValue)}>
+                                                    <Picker.Item label={'Please Select'} value={undefined} />
+                                                    <Picker.Item label="Malaysian Ringgit (MYR)" value="myr" />
+                                                    <Picker.Item label="Indonesian Rupiah (IDR)" value="idr" />
+                                                </Picker>
+                                            </View>}
                                         {recipientCurrencyError && recipientCurrencyTouched && <Text style={styles.error}>{recipientCurrencyTouched}</Text>}
                                         <TextInput value={recipientAmount} onChangeText={FormikProps.handleChange('recipientAmount')} onBlur={FormikProps.handleBlur('recipientAmount')} style={{ borderWidth: 1, borderColor: recipientAmountTouched && recipientAmountError ? '#d94498' : 'rgba(0,0,0,0.3)', padding: 5 }} placeholder={recipientAmountTouched && recipientAmountError ? '' : 'MYR 90.00'} placeholderTextColor={recipientAmountTouched && recipientAmountError ? 'rgba(255,0,0,0.3)' : 'lightgrey'} keyboardType={'phone-pad'} />
                                         {recipientAmountTouched && recipientAmountError && <Text style={styles.error}>{recipientAmountError}</Text>}
@@ -144,12 +190,12 @@ const RemittanceScreen = (props) => {
                                     </View>
                                 </ScrollView>
                             </View>
-                            <View style={{  flexDirection: 'row', alignSelf: 'stretch' }}>
+                            <View style={{ flexDirection: 'row', alignSelf: 'stretch' }}>
                                 <TouchableOpacity onPress={() => props.navigation.goBack()} style={{ flex: 1, borderColor: '#D3D3D3', borderWidth: 1, paddingTop: 20, paddingBottom: 20, justifyContent: 'center', alignItems: 'center' }}>
-                                        
-                                            <Text style={[styles.butang, { color: '#000000' }]}>Back</Text>
-                                        
-                                    </TouchableOpacity>
+
+                                    <Text style={[styles.butang, { color: '#000000' }]}>Back</Text>
+
+                                </TouchableOpacity>
                                 <TouchableOpacity disabled={!FormikProps.isValid} onPress={FormikProps.handleSubmit} style={{ flex: 1 }}>
                                     <LinearGradient colors={FormikProps.isValid ? ['#0A6496', '#055E7C'] : ['rgba(10,100,150,0.5)', 'rgba(5,94,124,0.5)']} style={{ flex: 1, padding: 10, justifyContent: 'center', alignItems: 'center' }}>
                                         {FormikProps.isSubmitting ? <ActivityIndicator color={'#fff'} /> :
