f9e37ebf3a215a72ed60f6e2839db170099ee5e4
Hafizh <eva_01@yahoo.com>
2020-02-06 17:21:10 +0800

    Buat dropdown button dkt Report screen & add amount,currency  dkt header box

--- src/screens/InvoiceScreen.js
+++ src/screens/InvoiceScreen.js
@@ -78,11 +78,21 @@ const InvoiceScreen = (props) => {
                                         <Ionicons name={item.marker ? "md-arrow-dropdown" : "md-arrow-dropright"} color={'#34C2DB'} style={{ fontSize: 25, paddingRight: 5 }} />
                                     </View>
                                 </TouchableWithoutFeedback>
-                                <View style={{ flexDirection: 'row', marginTop: 5, borderBottomWidth: item.marker ? 1 : 0, borderBottomColor: 'lightgrey', }}>
+                                <View style={{ flexDirection: 'row', marginTop: 5, borderBottomWidth: item.marker ? 1 : 0, borderBottomColor: 'lightgrey',paddingBottom:10 }}>
                                     <View style={{ flex: 1, paddingBottom: 5 }}>
                                         <Text style={styles.text}>{item.invoice_number}</Text>
                                     </View>
                                 </View>
+                                {!item.marker && <View style={{ flexDirection: 'row', marginTop: 5, }}>
+                                    <View style={{ flex: 1 }}>
+                                        <Text style={styles.small}>Currency</Text>
+                                        <Text style={styles.text}>{item.currency_code}</Text>
+                                    </View>
+                                    <View style={{ flex: 1 }}>
+                                        <Text style={styles.small}>Amount</Text>
+                                        <Text style={styles.text}>{item.amount}</Text>
+                                    </View>
+                                </View>}
                                 {item.marker && <View style={{ flex: 1 }}>
                                     <View style={{ flexDirection: 'row', marginTop: 20 }}>
                                         <View style={{ flex: 1 }}>
