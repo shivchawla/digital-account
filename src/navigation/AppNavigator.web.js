795ba3be7986a4c6f22b98cac8642c3455a7a91e
Syahrizan Ali Hassan <syahrizan@fintechcapital.co>
2020-02-06 17:41:52 +0800

    just discovered navigation for web and did necessary fixes

--- src/navigation/AppNavigator.web.js
+++ src/navigation/AppNavigator.web.js
@@ -1,13 +1,34 @@
 import { createBrowserApp } from '@react-navigation/web';
 import { createSwitchNavigator } from 'react-navigation';
 
+import AuthenticationNavigator from './AuthenticationNavigator'
+import RegistrationNavigator from './RegistrationNavigator'
 import MainTabNavigator from './MainTabNavigator';
 
-const switchNavigator = createSwitchNavigator({
-  // You could add another route here for authentication.
-  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
+const LoggedIn = createSwitchNavigator({
   Main: MainTabNavigator,
+  Authentication: AuthenticationNavigator,
+  Registration: RegistrationNavigator,
+ 
+}, {
+  initialRouteName: 'Main'
 });
-switchNavigator.path = '';
+LoggedIn.path = '';
 
-export default createBrowserApp(switchNavigator, { history: 'hash' });
+const Authentication = createSwitchNavigator({
+  Authentication: AuthenticationNavigator,
+  Registration: RegistrationNavigator,
+  Main: MainTabNavigator,
+
+}, {
+  initialRouteName: 'Authentication'
+});
+
+Authentication.path = '';
+
+export const LoggedInContainer = createBrowserApp(LoggedIn);
+
+export const AuthenticationContainer = createBrowserApp(Authentication);
+
+
+
