3c715232c509d8fe3e31a9b3b8d56b137caae334
Hafizh <eva_01@yahoo.com>
2020-02-12 11:51:23 +0800

    change borderradius at icon in loanapplication screen

--- src/screens/LoanApplicationScreen.js
+++ src/screens/LoanApplicationScreen.js
@@ -79,13 +79,13 @@ const LoanApplicationScreen = (props) => {
                                                     <View style={{ flex: 1, backgroundColor: '#CDCDCD' }} />
                                                 </View>
                                             </View>
-                                            <View style={{ width: 40, height: 40, borderRadius: 15, backgroundColor: '#20B8D3', justifyContent: 'center', alignItems: 'center' }}>
+                                            <View style={{ width: 40, height: 40, borderRadius: 20, backgroundColor: '#20B8D3', justifyContent: 'center', alignItems: 'center' }}>
                                                 <Ionicons name="md-card" color={'#fff'} style={{ fontSize: 15, }} />
                                             </View>
-                                            <View style={{ width: 40, height: 40, borderRadius: 15, backgroundColor: '#CDCDCD', justifyContent: 'center', alignItems: 'center' }}>
+                                            <View style={{ width: 40, height: 40, borderRadius: 20, backgroundColor: '#CDCDCD', justifyContent: 'center', alignItems: 'center' }}>
                                                 <Ionicons name="md-person" color={'grey'} style={{ fontSize: 15, }} />
                                             </View>
-                                            <View style={{ width: 40, height: 40, borderRadius: 15, backgroundColor: '#CDCDCD', justifyContent: 'center', alignItems: 'center' }}>
+                                            <View style={{ width: 40, height: 40, borderRadius: 20, backgroundColor: '#CDCDCD', justifyContent: 'center', alignItems: 'center' }}>
                                                 <Ionicons name="md-clipboard" color={'grey'} style={{ fontSize: 15, }} />
                                             </View>
                                         </View>
