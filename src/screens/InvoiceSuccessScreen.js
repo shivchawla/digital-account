93e9a021411b71c4f5f9193bfbf8e4ecdb0b87e0
Hafizh <eva_01@yahoo.com>
2020-02-13 16:50:42 +0800

    pending

--- src/screens/InvoiceSuccessScreen.js
+++ src/screens/InvoiceSuccessScreen.js
@@ -1,5 +1,5 @@
 import React,{useEffect} from 'react';
-import { Image, Text, TouchableOpacity, View, } from 'react-native';
+import { Image, Text, TouchableOpacity, View} from 'react-native';
 import Constants from 'expo-constants'
 import Layout from '../constants/Layout'
 import styles from '../styles/styles'
@@ -17,7 +17,7 @@ const InvoiceSuccessScreen = (props) => {
 
     return (
         <View style={{ flex: 1, paddingTop: Constants.statusBarHeight }}>
-            {true ? <View style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0, }}>
+            {status ? <View style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0, }}>
                 <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                 <View style={{ flex: 1, justifyContent: 'flex-end' }}>
                         <Image source={require('../assets/images/logo.png')} style={{ height: Layout.window.height * 0.2, width: Layout.window.width * 0.5 }} resizeMode={'contain'} />
