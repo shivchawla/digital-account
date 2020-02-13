e954003c53070fed77f999219f4fc75b9bc84c70
Hafizh <eva_01@yahoo.com>
2020-02-13 16:46:27 +0800

    fix btn issue at companyinfosuccess

--- src/screens/CompanyInfoSuccessScreen.js
+++ src/screens/CompanyInfoSuccessScreen.js
@@ -17,7 +17,7 @@ const CompanyInfoSuccessScreen = (props) => {
                     <Image source={require('../assets/images/merchantsuccess.png')} style={{ flex: 1, height: undefined, width: undefined }} resizeMode={'contain'} />
                 </View>
                 <View style={{ flex: 1, alignItems: 'center' }}>
-                    <Text style={[styles.h3, { margin: 5, fontWeight: 'bold' }]}>Info Submitted!</Text>
+                    <Text style={[styles.h3, { margin: 5, fontWeight: 'bold',fontSize:17 }]}>Info Submitted!</Text>
                     <View style={{ alignSelf: 'stretch', flexDirection: 'column', margin: 5, alignItems: 'center' }}>
                         <Text style={[styles.text, { margin: 5, color: 'darkturquoise' }]}>Congratulation!</Text>
                         <Text style={[styles.text, { margin: 5, marginBottom: 20, textAlign: 'center' }]}>You have entered your company information. You can either choose to enter contact info or skip to the dashboard.</Text>
@@ -25,10 +25,10 @@ const CompanyInfoSuccessScreen = (props) => {
                 </View>
                 <View style={{ flex: 1 }}>
                     <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-                    <TouchableOpacity onPress={() => props.navigation.navigate('Dashboard')} style={{ width: Layout.window.width * 0.3, paddingTop: 5, paddingBottom: 5, borderRadius: 15, justifyContent: 'center', alignItems: 'center', margin: 10, borderColor: 'black', borderWidth: 1 }}>
+                    <TouchableOpacity onPress={() => props.navigation.navigate('Dashboard')} style={{ width: Layout.window.width * 0.3, paddingTop: 16, paddingBottom: 16, borderRadius: 15, justifyContent: 'center', alignItems: 'center', margin: 10, borderColor: 'black', borderWidth: 1 }}>
                                 <Text style={[styles.textDefault]}>Skip</Text>
                             </TouchableOpacity>
-                            <TouchableOpacity onPress={() => props.navigation.navigate('ContactPerson')} style={{ width: Layout.window.width * 0.3, paddingTop: 5, paddingBottom: 5, borderRadius: 15, justifyContent: 'center', alignItems: 'center', margin: 10, backgroundColor: '#09A4BF' }}>
+                            <TouchableOpacity onPress={() => props.navigation.navigate('ContactPerson')} style={{ width: Layout.window.width * 0.3, paddingTop: 16, paddingBottom: 16, borderRadius: 15, justifyContent: 'center', alignItems: 'center', margin: 10, backgroundColor: '#09A4BF' }}>
                                 <Text style={[styles.textDefault, { color: 'white' }]}>Contact Info</Text>
                             </TouchableOpacity>
                     </View>
