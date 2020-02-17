659242958b2615275d88ae4d603dcf776b37b0db
Hafizh <eva_01@yahoo.com>
2020-02-17 15:08:36 +0800

    change btn size at filterbarinvoice

--- src/components/FilterBarInvoice.js
+++ src/components/FilterBarInvoice.js
@@ -99,10 +99,10 @@ const FilterBarInvoice = (props) => {
                 </View>
             </View>
             <View style={{ flexDirection: 'row', margin: 5, justifyContent: 'center' }}>
-                <TouchableOpacity onPress={() => {nav('Invoice')}}  style={{ width: Layout.window.width * 0.3, paddingTop: 5, paddingBottom: 5, borderRadius: 15, justifyContent: 'center', alignItems: 'center', margin: 10, borderColor: 'black', borderWidth: 1 }}>
+                <TouchableOpacity onPress={() => {nav('Invoice')}}  style={{ width: Layout.window.width * 0.2, paddingTop: 16, paddingBottom: 16, borderRadius: 15, justifyContent: 'center', alignItems: 'center', margin: 10, borderColor: 'black', borderWidth: 1 }}>
                     <Text style={[styles.textDefault, { color: 'black' }]}>Reset</Text>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() => {filterInvoicesList();nav('Invoice')}} style={{ width: Layout.window.width * 0.3, paddingTop: 5, paddingBottom: 5, borderRadius: 15, justifyContent: 'center', alignItems: 'center', margin: 10, backgroundColor: '#09A4BF' }} >
+                <TouchableOpacity onPress={() => {filterInvoicesList();nav('Invoice')}} style={{ width: Layout.window.width * 0.2, paddingTop: 16, paddingBottom: 16, borderRadius: 15, justifyContent: 'center', alignItems: 'center', margin: 10, backgroundColor: '#09A4BF' }} >
                     <Text style={[styles.textDefault, { color: '#fff' }]}>Filter</Text>
                 </TouchableOpacity>
             </View>
