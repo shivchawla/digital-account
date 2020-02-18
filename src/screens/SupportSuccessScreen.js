f0ea9ec8b8b178ab467b7adf03da5cd368d27112
Hafizh <eva_01@yahoo.com>
2020-02-18 11:09:35 +0800

    change border color for support success screen

--- src/screens/SupportSuccessScreen.js
+++ src/screens/SupportSuccessScreen.js
@@ -16,7 +16,7 @@ const SupportSuccessScreen = (props) => {
     return (
 
         <View style={{ flex: 1, paddingTop: Constants.statusBarHeight }}>
-            {status ? <View style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0, }}>
+            {true? <View style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0, }}>
                 <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                 <View style={{ flex: 1, justifyContent: 'flex-end' }}>
                         <Image source={require('../assets/images/logo.png')} style={{ height: Layout.window.height * 0.2, width: Layout.window.width * 0.5 }} resizeMode={'contain'} />
@@ -33,7 +33,7 @@ const SupportSuccessScreen = (props) => {
                     </View>
                     <View style={{ flex: 1 }}>
                         <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-                            <TouchableOpacity onPress={() => {resetCode();props.navigation.navigate('Dashboard')}} style={{ width: Layout.window.width * 0.3, paddingTop: 16, paddingBottom: 16, borderRadius: 15, justifyContent: 'center', alignItems: 'center', margin: 10, borderColor: 'darkturqoise', borderWidth: 1 }}>
+                            <TouchableOpacity onPress={() => {resetCode();props.navigation.navigate('Dashboard')}} style={{ width: Layout.window.width * 0.3, paddingTop: 16, paddingBottom: 16, borderRadius: 15, justifyContent: 'center', alignItems: 'center', margin: 10, borderColor: 'darkturquoise', borderWidth: 1 }}>
                                 <Text style={[styles.textDefault]}>Dashboard</Text>
                             </TouchableOpacity>
                             <TouchableOpacity onPress={() => {resetCode();props.navigation.navigate('Support')}} style={{ width: Layout.window.width * 0.3, paddingTop: 16, paddingBottom: 16, borderRadius: 15, justifyContent: 'center', alignItems: 'center', margin: 10, backgroundColor: '#09A4BF' }}>
@@ -59,7 +59,7 @@ const SupportSuccessScreen = (props) => {
                     </View>
                     <View style={{ flex: 1 }}>
                         <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-                            <TouchableOpacity onPress={() => {resetCode();props.navigation.navigate('Dashboard')}} style={{ width: Layout.window.width * 0.3, paddingTop: 16, paddingBottom: 16, borderRadius: 15, justifyContent: 'center', alignItems: 'center', margin: 10, borderColor: 'darkturqoise', borderWidth: 1 }}>
+                            <TouchableOpacity onPress={() => {resetCode();props.navigation.navigate('Dashboard')}} style={{ width: Layout.window.width * 0.3, paddingTop: 16, paddingBottom: 16, borderRadius: 15, justifyContent: 'center', alignItems: 'center', margin: 10, borderColor: 'darkturquoise', borderWidth: 1 }}>
                                 <Text style={[styles.textDefault]}>Dashboard</Text>
                             </TouchableOpacity>
                             <TouchableOpacity onPress={() => {resetCode();props.navigation.navigate('Support')}} style={{ width: Layout.window.width * 0.3, paddingTop: 16, paddingBottom: 16, borderRadius: 15, justifyContent: 'center', alignItems: 'center', margin: 10, backgroundColor: '#09A4BF' }}>
