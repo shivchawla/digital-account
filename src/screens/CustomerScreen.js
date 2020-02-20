dfebf0d7601c3b9b0748a66e9ea34c16b5d68369
Hafizh <eva_01@yahoo.com>
2020-02-20 15:52:09 +0800

    change to custom component such as layoutA and customButton at customer screen

--- src/screens/CustomerScreen.js
+++ src/screens/CustomerScreen.js
@@ -5,7 +5,8 @@ import { shallowEqual, useSelector, useDispatch } from 'react-redux'
 import { Ionicons } from '@expo/vector-icons';
 import moment from 'moment'
 import styles from '../styles/styles'
-
+import { CustomButton } from '../components/Custom'
+import LayoutA from '../Layout/LayoutA';
 const CustomerScreen = (props) => {
 
     useEffect(() => {
@@ -16,28 +17,21 @@ const CustomerScreen = (props) => {
 
     return (
 
-        <View style={{ flex: 1, }}>
-            <View style={[styles.titleMargin, { flex: 1, flexDirection: 'row', borderBottomWidth: 1, borderColor: '#9ADAF4' }]}>
-                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start' }}>
-                    <TouchableOpacity onPress={() => props.navigation.navigate('DataSetting')} hitslop={{ top: 20, left: 20, bottom: 20, right: 20 }}>
-                        <Ionicons name="ios-arrow-back" color={'#3EC2D9'} style={{ fontSize: 30 }} />
-                    </TouchableOpacity>
-                </View>
-                <View style={{ flex: 5, justifyContent: 'center', alignItems: 'center' }}>
-                    <Text style={[styles.title]}>CUSTOMER LIST</Text>
-                </View>
-                <TouchableOpacity onPress={() => props.navigation.navigate('EditProfile')} style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-end' }}>
-                    <View style={{ backgroundColor: 'rgba(62,194,217,0.5)', borderColor: "#3EC2D9", borderWidth: 0, width: 36, height: 36, borderRadius: 18, justifyContent: 'center', alignItems: 'center' }}>
-                        <Ionicons name="md-person" color={'#fff'} style={{ fontSize: 25 }} />
-                    </View>
-                </TouchableOpacity>
-            </View>
+
+        < LayoutA
+            title={'CUSTOMER LIST'}
+            screenType='form'
+            navigation={props.navigation}
+            nopadding
+        >
             <View style={[{ flex: 9 }]}>
                 <View style={[styles.screenMargin]}>
                     <View style={{ marginTop: 10, flexDirection: 'row', alignSelf: 'stretch', justifyContent: 'flex-end', paddingRight: 10 }}>
-                        <TouchableOpacity onPress={() => props.navigation.navigate('CustomerAdd')} style={{ paddingTop: 17, paddingBottom: 17, paddingLeft: 19, paddingRight: 19, backgroundColor: '#34C2DB', borderRadius: 15 }}>
-                            <Text style={[styles.text, { color: '#fff' }]}>Add Customer</Text>
-                        </TouchableOpacity>
+                        <CustomButton
+                            navigation={() => props.navigation.navigate('CustomerAdd')}
+                            label={'Add Customer'}
+                            boxStyle={{ backgroundColor: '#34C2DB' }}
+                        />
                     </View>
                     <View style={{ marginTop: 20 }}>
                         <View style={{ flexDirection: 'row', justifyContent: 'flex-end', paddingRight: 10 }}>
@@ -53,7 +47,7 @@ const CustomerScreen = (props) => {
                         </View>
                         <ScrollView>
                             {customerList && <FlatList data={filterEnabled ? filteredCustomerList : customerList} keyExtractor={(item, index) => index.toString()} renderItem={({ item }) =>
-                                <View  style={styles.box}>
+                                <View style={styles.box}>
                                     <View style={{ flexDirection: 'row', marginTop: 5 }}>
                                         <View style={{ flex: 1, flexDirection: 'row', alignSelf: 'stretch', justifyContent: 'space-between' }}>
                                             <Text style={styles.small}>{moment(item.created_at).format("MMMM Do YYYY, h:mm:ss a")}</Text>
@@ -81,7 +75,7 @@ const CustomerScreen = (props) => {
                     </View>
                 </View>
             </View >
-        </View >
+       </ LayoutA>
 
     );
 }
