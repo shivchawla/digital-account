f348a83cf5382f8c748770fdabc7eb2f7637ea09
Syahrizan Ali Hassan <syahrizan@fintechcapital.co>
2020-02-24 09:15:31 +0800

    still working on auth options

--- src/screens/AuthOptionScreen.js
+++ src/screens/AuthOptionScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, TouchableOpacity, Text, Image, Switch,Modal } from 'react-native';
+import { View, TouchableOpacity, Text, Image, Switch, Modal } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import styles from '../styles/styles'
 import * as LocalAuthentication from 'expo-local-authentication';
@@ -16,14 +16,14 @@ const AuthOptionTypeScreen = (props) => {
 
     const unlock = () => {
         setLock(false)
-        dispatch(actionCreator.setAuth({ authEnabled: false }))
+        dispatch(actionCreator.savePin({ authEnabled: false,authType:'NA' }))
         setAuthRequestVisible(false)
     }
 
     const checkCode = (code) => {
         console.log(`periksa code`)
         return code === '1234'
-       // return code === pin
+        // return code === pin
     }
 
     const [locked, setLock] = useState(true)
@@ -40,19 +40,23 @@ const AuthOptionTypeScreen = (props) => {
     //     props.navigation.navigate('SetPasscode')
     // }
 
-    const { authEnabled, authType,pin } = useSelector(state => state.authReducer, shallowEqual)
+    const { authEnabled, authType, pin } = useSelector(state => state.authReducer, shallowEqual)
     const allAuth = useSelector(state => state.authReducer, shallowEqual)
 
+    allAuth && console.log(`all auth ialah ${JSON.stringify(allAuth)}`)
+    authEnabled && console.log(`authEnabled ialah ${JSON.stringify(authEnabled)}`)
 
-    const [notificationEnable, setNotificationEnable] = useState(authEnabled)
+    const [notificationEnable, setNotificationEnable] = authEnabled && useState(authEnabled)
 
     const notificationToggle = () => {
-    
-        if (authEnabled) {
+
+        if (authEnabled==1) {
             //buka dulu authentication screen
             setAuthRequestVisible(true)
-            
+            setNotificationEnable(!notificationEnable)
+
         } else {
+            setNotificationEnable(!notificationEnable)
             dispatch(actionCreator.setAuth({ authEnabled: !notificationEnable }))
             props.navigation.navigate('AuthOptionType')
             //setNotificationEnable(value)
@@ -61,7 +65,7 @@ const AuthOptionTypeScreen = (props) => {
 
     return (
         <View style={{ flex: 1, }}>
-            {/* <Modal transparent={true} animationType={'slide'} visible={authRequestVisible} onRequestClose={() => setAuthRequestVisible(!authRequestVisible)} >
+            <Modal transparent={true} animationType={'slide'} visible={authRequestVisible} onRequestClose={() => setAuthRequestVisible(!authRequestVisible)} >
                 <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(1,1,1,0.5)' }}>
                     <View style={{ flex: 3 }} />
                     <View style={{ flex: 1, backgroundColor: '#fff', justifyContent: 'center', alignItems: 'center' }}>
@@ -73,14 +77,14 @@ const AuthOptionTypeScreen = (props) => {
                             error="Try again" // If user fail (fill '2017' for instance)
                             autoFocusFirst={true} // disabling auto-focus
                             keyboardType={'numeric'}
-                            containerStyle={{ width: Layout.window.width, height: Layout.window.height / 4,justifyContent:'flex-start',alignItems:'center' }}
+                            containerStyle={{ width: Layout.window.width, height: Layout.window.height / 4, justifyContent: 'flex-start', alignItems: 'center' }}
                         //     containerPinStyle={{ width:40, height: 40, flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', marginTop: 20 }
                         // }
                         /> : <ScanFinger unlock={unlock} />}
 
                     </View>
                 </View>
-            </Modal> */}
+            </Modal>
             <View style={{ flex: 1, flexDirection: 'row', borderBottomWidth: 1, borderColor: '#9ADAF4' }}>
                 <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start', marginLeft: 0 }}>
                     <TouchableOpacity onPress={() => props.navigation.navigate("DataSetting")} hitslop={{ top: 20, left: 20, bottom: 20, right: 20 }}>
@@ -98,15 +102,15 @@ const AuthOptionTypeScreen = (props) => {
             </View>
             <View style={[styles.screenMargin, { flex: 9 }]}>
                 <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-
+                    <Text>ADA:{authEnabled}</Text>
                     <View>
                         <View style={{ flexDirection: 'row', marginTop: 30 }}>
                             <Text style={[styles.text], { padding: 5 }}>Enable Authentication</Text>
-                            <Switch style={{ marginLeft: 35, marginRight: 15 }} onValueChange={value => notificationToggle(value)} value={authEnabled} />
+                            <Switch style={{ marginLeft: 35, marginRight: 15 }} onValueChange={value => notificationToggle(value)} value={authEnabled ==1} />
                         </View>
-                        {authEnabled && <View style={{ flexDirection: 'row', marginTop: 30 }}>
+                        {authEnabled ? <View style={{ flexDirection: 'row', marginTop: 30 }}>
                             <Text style={[styles.text], { padding: 5 }}>Authentication Type : {authType}</Text>
-                        </View>}
+                        </View> : <View />}
                     </View>
                 </View>
             </View >
