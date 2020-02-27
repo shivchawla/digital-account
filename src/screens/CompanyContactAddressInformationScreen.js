6568a4d7b7fa0fd1a954e0769c4c089d64f2051b
Hafizh <eva_01@yahoo.com>
2020-02-27 17:09:27 +0800

    change to LayoutA,customtextinput and customformaction at companycontactaddress

--- src/screens/CompanyContactAddressInformationScreen.js
+++ src/screens/CompanyContactAddressInformationScreen.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect}from 'react';
+import React, { useState, useEffect } from 'react';
 import { Image, Text, TouchableOpacity, View, TextInput, KeyboardAvoidingView, ActivityIndicator, ScrollView, Picker } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient'
 import { Formik } from 'formik';
@@ -7,7 +7,9 @@ import styles from '../styles/styles'
 import { shallowEqual, useSelector, useDispatch } from 'react-redux'
 import * as actionCreator from '../store/actions/action'
 import malaysiaData from 'malaysia-city-postcode'
-import {keyboardBeingDisplay,keyboardBeingClose} from '../components/handleKeyboard'
+import { keyboardBeingDisplay, keyboardBeingClose } from '../components/handleKeyboard'
+import { CustomFormAction, CustomTextInput } from '../components/Custom'
+import LayoutA from '../Layout/LayoutA';
 
 const validationSchema = Yup.object().shape({
 
@@ -50,11 +52,11 @@ const CompanyContactAddressInformationScreen = (props) => {
     // const found = newAddress.find(n => n.Postcode === '73400')
     // console.log(`Ini Hasil Pertama  ${found.State}`)
 
-    const [offSet,setOffSet]=useState(true)
+    const [offSet, setOffSet] = useState(true)
     useEffect(() => {
-        const open=()=>setOffSet(false)
-        const off=()=>setOffSet(true)
-       
+        const open = () => setOffSet(false)
+        const off = () => setOffSet(true)
+
         keyboardBeingDisplay(open)
         keyboardBeingClose(off)
     }, []); // empty-array means don't watch for any updates
@@ -113,43 +115,69 @@ const CompanyContactAddressInformationScreen = (props) => {
 
                 return (
 
-                    <KeyboardAvoidingView behavior="padding" enabled style={{ flex: 2 }} keyboardVerticalOffset={offSet?30:0}>
-                        <View style={[styles.titleMargin, { flex: 1, flexDirection: 'row', borderBottomWidth: 1, borderColor: '#9ADAF4', marginBottom: 25 }]}>
-                            <View style={{ flex: 3, justifyContent: 'center', alignItems: 'center', alignItems: 'flex-start', paddingLeft: 10 }}>
-                                <Text numberOfLines={1} style={styles.title} ellipsizeMode='tail'>ADDRESS</Text>
-                            </View>
-                            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-end' }}>
-                                <Image source={require('../assets/images/logosmall.png')} style={{ width: 50, height: 50, borderRadius: 15 }} />
-                            </View>
-                        </View>
-                        <View style={{ justifyContent: 'space-between', flex: 9 }}>
+                    <KeyboardAvoidingView behavior="padding" enabled style={{ flex: 2 }} keyboardVerticalOffset={offSet ? 30 : 0}>
+                        <LayoutA
+                            title={'ADDRESS'}
+                            screenType='registration'
+                            navigation={props.navigation}
+                            nopadding
+                        >
+
                             <View style={[{ flex: 9 }]}>
                                 <ScrollView style={[styles.screenMargin]}>
-                                    <View style={[styles.formElement]}>
-                                        <Text style={[styles.titleBox, { marginBottom: 5, borderBottomColor: comp_addrTouched && comp_addrError ? 'rgba(255,0,0,1)' : '#5a83c2' }]}>Address Line 1</Text>
-                                        <TextInput value={comp_addr} onBlur={FormikProps.handleBlur('comp_addr')} onChangeText={FormikProps.handleChange('comp_addr')} placeholder={comp_addrTouched && comp_addrError ? '' : 'Eg: 89, Jalan Bestari'} style={[styles.textInput,{ borderWidth: 1, borderColor: 'rgba(0,0,0,0.3)', padding: 5 }]} />
-                                        {comp_addrTouched && comp_addrError && <Text style={styles.error}>{comp_addrError}</Text>}
-                                    </View>
-                                    <View style={[styles.formElement]}>
-                                        <Text style={[styles.titleBox, { marginBottom: 5, borderBottomColor: comp_addr2Touched && comp_addr2Error ? 'rgba(255,0,0,1)' : '#5a83c2' }]}>Line Address 2</Text>
-                                        <TextInput value={comp_addr2} onBlur={FormikProps.handleBlur('comp_addr2')} onChangeText={FormikProps.handleChange('comp_addr2')} placeholder={comp_addr2Touched && comp_addr2Error ? '' : 'Eg: Taman Enggang Utama'} style={[styles.textInput,{ borderWidth: 1, borderColor: 'rgba(0,0,0,0.3)', padding: 5 }]} />
-                                        {comp_addr2Touched && comp_addr2Error && <Text style={styles.error}>{comp_addr2Error}</Text>}
-                                    </View>
-                                    <View style={[styles.formElement]}>
-                                        <Text style={[styles.titleBox, { marginBottom: 5, borderBottomColor: cddPostcodeTouched && cddPostcodeError ? 'rgba(255,0,0,1)' : '#5a83c2' }]}>Postcode</Text>
-                                        <TextInput value={cddPostcode} onBlur={FormikProps.handleBlur('cddPostcode')} onChangeText={(val) => checkPostcode(val)} placeholder={cddPostcodeTouched && cddPostcodeError ? '' : 'Eg: 60901'} style={[styles.textInput,{ borderWidth: 1, borderColor: 'rgba(0,0,0,0.3)', padding: 5 }]} keyboardType={'phone-pad'} />
-                                        {cddPostcodeTouched && cddPostcodeError && <Text style={styles.error}>{cddPostcodeError}</Text>}
-                                    </View>
-                                    <View style={[styles.formElement]}>
-                                        <Text style={[styles.titleBox, { marginBottom: 5 }]}>City</Text>
-                                        <TextInput editable={false} value={comp_city} onChangeText={FormikProps.handleChange('comp_city')} onBlur={FormikProps.handleBlur('comp_city')} style={[styles.textInput,{ borderWidth: 1, borderColor: comp_cityTouched && comp_cityError ? 'rgba(255,0,0,1)' : 'rgba(0,0,0,0.3)', padding: 5 }]} placeholder={comp_cityTouched && comp_cityError ? '' : ''} placeholderTextColor={comp_cityTouched && comp_cityError ? 'rgba(255,0,0,0.3)' : 'lightgrey'} />
-                                        {comp_cityTouched && comp_cityError && <Text style={styles.error}>{comp_cityError}</Text>}
-                                    </View>
-                                    <View style={[styles.formElement]}>
-                                        <Text style={[styles.titleBox, { marginBottom: 5 }]}>State</Text>
-                                        <TextInput editable={false} value={comp_state} onChangeText={FormikProps.handleChange('comp_state')} onBlur={FormikProps.handleBlur('comp_state')} style={[styles.textInput,{ borderWidth: 1, borderColor: comp_stateTouched && comp_stateError ? 'rgba(255,0,0,1)' : 'rgba(0,0,0,0.3)', padding: 5 }]} placeholder={comp_stateTouched && comp_stateError ? '' : ''} placeholderTextColor={comp_stateTouched && comp_stateError ? 'rgba(255,0,0,0.3)' : 'lightgrey'} />
-                                        {comp_stateTouched && comp_stateError && <Text style={styles.error}>{comp_stateError}</Text>}
-                                    </View>
+                                    <CustomTextInput
+                                        label={`Address Line 1`}
+                                        value={comp_addr}
+                                        handleChange={FormikProps.handleChange(`comp_addr`)}
+                                        handleBlur={FormikProps.handleBlur(`comp_addr`)}
+                                        touched={comp_addrTouched}
+                                        error={comp_addrError}
+                                        placeholder={'Eg: 89, Jalan Bestari'}
+
+                                    />
+                                    <CustomTextInput
+                                        label={`Line Address 2`}
+                                        value={comp_addr2}
+                                        handleChange={FormikProps.handleChange(`comp_addr2`)}
+                                        handleBlur={FormikProps.handleBlur(`comp_addr2`)}
+                                        touched={comp_addr2Touched}
+                                        error={comp_addr2Error}
+                                        placeholder={'Eg: Taman Enggang Utama'}
+
+                                    />
+                                    <CustomTextInput
+                                        label={`Postcode`}
+                                        value={cddPostcode}
+                                        handleChange={FormikProps.handleChange(`cddPostcode`)}
+                                        handleBlur={FormikProps.handleBlur(`cddPostcode`)}
+                                        touched={cddPostcodeTouched}
+                                        error={cddPostcodeError}
+                                        placeholder={'Eg: 60901'}
+                                        keyboardType={'phone-pad'}
+                                    />
+                                    <CustomTextInput
+                                        label={`City`}
+                                        value={comp_city}
+                                        handleChange={FormikProps.handleChange(`comp_city`)}
+                                        handleBlur={FormikProps.handleBlur(`comp_city`)}
+                                        touched={comp_cityTouched}
+                                        error={comp_cityError}
+                                        placeholder={''}
+                                       
+                                        
+                                    />
+                                     <CustomTextInput
+                                        label={`State`}
+                                        value={comp_state}
+                                        handleChange={FormikProps.handleChange(`comp_state`)}
+                                        handleBlur={FormikProps.handleBlur(`comp_state`)}
+                                        touched={comp_stateTouched}
+                                        error={comp_stateError}
+                                        placeholder={''}
+                                        
+                                        
+                                    />
+                            
                                     {/* <View style={[styles.formElement, { alignSelf: 'stretch' }]}>
                                         <Text style={[styles.titleBox, { marginBottom: 10 }]}>State</Text>
                                         <View style={{ alignSelf: 'stretch', borderWidth: 1, borderColor: 'rgba(0,0,0,0.3)' }}>
@@ -175,19 +203,14 @@ const CompanyContactAddressInformationScreen = (props) => {
                                     </View> */}
                                 </ScrollView>
                             </View>
-                            <View style={{ flexDirection: 'row', alignSelf: 'stretch' }}>
-                                <TouchableOpacity onPress={() => props.navigation.goBack()} style={{ flex: 1, borderColor: '#D3D3D3', borderWidth: 1,paddingTop: 20, paddingBottom: 20, justifyContent: 'center', alignItems: 'center' }}>
-                                  
-                                        <Text style={[styles.butang, { color: 'lightgrey' }]}>Back</Text>
-                               
-                                </TouchableOpacity>
-                                <TouchableOpacity disabled={!FormikProps.isValid} onPress={FormikProps.handleSubmit} style={{ flex: 1, borderColor: FormikProps.isValid ? '#0A6496' : 'rgba(10,100,150,0.5)', borderWidth: 1 }}>
-                                    <LinearGradient colors={FormikProps.isValid ? ['#0A6496', '#055E7C'] : ['rgba(10,100,150,0.5)', 'rgba(5,94,124,0.5)']} style={{ flex: 1, padding: 10, justifyContent: 'center', alignItems: 'center' }}>
-                                        {FormikProps.isSubmitting ? <ActivityIndicator color={'#fff'} /> : <Text style={[styles.butang, { color: '#fff' }]}>Confirm</Text>}
-                                    </LinearGradient>
-                                </TouchableOpacity>
-                            </View>
-                        </View>
+                            <CustomFormAction
+                                navigation={props.navigation}
+                                isValid={FormikProps.isValid}
+                                handleSubmit={FormikProps.handleSubmit}
+                                isSubmitting = {FormikProps.isSubmitting}
+                                label={`Save`}
+                            />
+                        </LayoutA>
                     </KeyboardAvoidingView>)
             }}
         </Formik >
