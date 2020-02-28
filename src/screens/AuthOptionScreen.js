81ed9f66233a5b16e765e44d2a322a37eac86ecd
Hafizh <eva_01@yahoo.com>
2020-02-28 16:45:40 +0800

    change to layout A at AuthOptionScreen

--- src/screens/AuthOptionScreen.js
+++ src/screens/AuthOptionScreen.js
@@ -8,7 +8,7 @@ import { shallowEqual, useSelector, useDispatch } from 'react-redux'
 
 import CodePin from 'react-native-pin-code'
 import Layout from '../constants/Layout';
-
+import LayoutA from '../Layout/LayoutA';
 import ScanFinger from '../components/ScanFinger'
 const AuthOptionScreen = (props) => {
 
@@ -152,21 +152,14 @@ const AuthOptionScreen = (props) => {
                 </View>
             </Modal> : <View />}
 
-            <View style={{ flex: 1, flexDirection: 'row', borderBottomWidth: 1, borderColor: '#9ADAF4' }}>
-                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start', marginLeft: 0 }}>
-                    <TouchableOpacity onPress={() => props.navigation.navigate("DataSetting")} hitslop={{ top: 20, left: 20, bottom: 20, right: 20 }}>
-                        <Ionicons name="ios-arrow-back" color={'#3EC2D9'} style={{ fontSize: 30, paddingLeft: 20 }} />
-                    </TouchableOpacity>
-                </View>
-                <View style={{ flex: 3, justifyContent: 'center', alignItems: 'center' }}>
-                    <Text numberOfLines={1} style={styles.title} ellipsizeMode='tail'>AUTHENTICATION SETTING</Text>
-                </View>
-                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-end', marginRight: 10 }}>
-                    <View style={{ backgroundColor: 'rgba(62,194,217,0.5)', borderColor: "#3EC2D9", borderWidth: 0, width: 36, height: 36, borderRadius: 18, justifyContent: 'center', alignItems: 'center' }}>
-                        <Ionicons name="md-person" color={'#fff'} style={{ fontSize: 25 }} />
-                    </View>
-                </View>
-            </View>
+             <LayoutA
+                            title={'AUTHENTICATION SETTING'}
+                            screenType='form'
+                            navigation={props.navigation}
+                            nopadding
+                        >
+
+            
             <View style={[styles.screenMargin, { flex: 9 }]}>
                 <View style={{ justifyContent: 'center', alignItems: 'center' }}>
 
@@ -181,7 +174,8 @@ const AuthOptionScreen = (props) => {
                     </View>
                 </View>
             </View >
-        </View >
+        </LayoutA>
+        </View>
     );
 }
 
