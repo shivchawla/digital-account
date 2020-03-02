39870968d355416c276c2ad36cd7c39852bef9ce
Hafizh <eva_01@yahoo.com>
2020-03-02 17:21:43 +0800

    add layout A to edit profile screen

--- src/screens/EditProfileScreen.js
+++ src/screens/EditProfileScreen.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, TouchableOpacity, Text, Image, ActivityIndicator, KeyboardAvoidingView, TextInput, ScrollView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient'
 import { Ionicons } from '@expo/vector-icons';
@@ -7,7 +7,10 @@ import * as Yup from 'yup';
 import * as actionCreator from '../store/actions/action'
 import { shallowEqual, useSelector, useDispatch } from 'react-redux'
 import styles from '../styles/styles'
-import {keyboardBeingDisplay,keyboardBeingClose} from '../components/handleKeyboard'
+import { keyboardBeingDisplay, keyboardBeingClose } from '../components/handleKeyboard'
+import LayoutA from '../Layout/LayoutA';
+import { CustomFormAction, CustomTextInput } from '../components/Custom'
+
 
 const validationSchema = Yup.object().shape({
 
@@ -42,11 +45,11 @@ const EditProfileScreen = (props) => {
         retrieveMerchantInfo()
     }, [])
 
-    const [offSet,setOffSet]=useState(true)
+    const [offSet, setOffSet] = useState(true)
     useEffect(() => {
-        const open=()=>setOffSet(false)
-        const off=()=>setOffSet(true)
-       
+        const open = () => setOffSet(false)
+        const off = () => setOffSet(true)
+
         keyboardBeingDisplay(open)
         keyboardBeingClose(off)
     }, []); // empty-array means don't watch for any updates
@@ -66,76 +69,77 @@ const EditProfileScreen = (props) => {
 
                 return (
 
-                    <KeyboardAvoidingView behavior="padding" enabled style={{ flex: 1, }} keyboardVerticalOffset={offSet?30:0} >
-                    
-                    <View style={{ flex: 1 }}>
-                        <View style={{ flex: 1, flexDirection: 'row', borderBottomWidth: 1, borderColor: '#9ADAF4' }}>
-                            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start', marginLeft: 0 }}>
-                                <TouchableOpacity onPress={() => props.navigation.goBack()} hitslop={{ top: 20, left: 20, bottom: 20, right: 20 }}>
-                                    <Ionicons name="ios-arrow-back" color={'#3EC2D9'} style={{ fontSize: 30, paddingLeft: 20 }} />
-                                </TouchableOpacity>
-                            </View>
-                            <View style={{ flex: 3, justifyContent: 'center', alignItems: 'center' }}>
-                                <Text style={[styles.title, { color: '#055E7C' }]}>EDIT PROFILE</Text>
-                            </View>
-                            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-end', marginRight: 10 }}>
-                                <View style={{ backgroundColor: 'rgba(62,194,217,0.5)', borderColor: "#3EC2D9", borderWidth: 0, width: 36, height: 36, borderRadius: 18, justifyContent: 'center', alignItems: 'center' }}>
-                                    <Ionicons name="md-person" color={'#fff'} style={{ fontSize: 25 }} />
-                                </View>
-                            </View>
-                        </View>
-                        <View style={{ justifyContent: 'space-between', flex: 9 }}>
-                            <View style={[{ flex: 9 }]}>
-                                <ScrollView style={[styles.screenMargin]}>
-                                <View style={{ flexDirection: 'row', justifyContent: 'flex-start', marginBottom: 10, marginTop: 25, marginBottom: 25 }}>
+                    <KeyboardAvoidingView behavior="padding" enabled style={{ flex: 1, }} keyboardVerticalOffset={offSet ? 30 : 0} >
+
+                        <View style={{ flex: 1 }}>
+                            <LayoutA
+                                title={'EDIT PROFILE'}
+                                screenType='form'
+                                navigation={props.navigation}
+                                nopadding
+                            >
+
+                                <View style={[{ flex: 9 }]}>
+                                    <ScrollView style={[styles.screenMargin]}>
+                                        <View style={{ flexDirection: 'row', justifyContent: 'flex-start', marginBottom: 10, marginTop: 25, marginBottom: 25 }}>
                                             <View style={{ backgroundColor: 'rgba(62,194,217,0.5)', borderColor: "#3EC2D9", borderWidth: 1, width: 90, height: 90, justifyContent: 'center', alignItems: 'center' }}>
                                                 <Ionicons name="md-person" color={'#fff'} style={{ fontSize: 60 }} />
                                             </View>
                                         </View>
-                                        <View style={[styles.formElement]}>
-                                            <Text style={[styles.titleBox, { marginBottom: 10 }]}>Company Name</Text>
-                                            <TextInput value={companyName} onChangeText={FormikProps.handleChange('companyName')} style={[styles.textInput,{ borderWidth: 1, borderColor: 'rgba(0,0,0,0.3)', padding: 5 }]} placeholder={'Eg: Syarikat Maju Budi Sdn Bhd'} />
-                                        </View>
-                                        <View style={[styles.formElement]}>
-                                            <Text style={[styles.titleBox, { marginBottom: 10 }]}>Registration Number</Text>
-                                            <TextInput value={companyRegNum} onChangeText={FormikProps.handleChange('companyRegNum')} style={[styles.textInput,{ borderWidth: 1, borderColor: 'rgba(0,0,0,0.3)', padding: 5 }]} placeholder={'Eg: 543-A123'} />
-                                        </View>
-                                        <View style={[styles.formElement]}>
-                                            <Text style={[styles.titleBox, { marginBottom: 10 }]}>Company Addresss</Text>
-                                            <TextInput value={companyAddress} onChangeText={FormikProps.handleChange('companyAddress')} style={[styles.textInput,{ borderWidth: 1, borderColor: 'rgba(0,0,0,0.3)', padding: 5, }]} placeholder={'Eg: 86, Jalan Budiman, Kuala Langat, Selangor'} />
-                                        </View>
-                                        <View style={[styles.formElement]}>
-                                            <Text style={[styles.titleBox, { marginBottom: 10 }]}>Postcode</Text>
-                                            <TextInput value={companyPostcode} onChangeText={FormikProps.handleChange('companyPostcode')} style={[styles.textInput,{ borderWidth: 1, borderColor: 'rgba(0,0,0,0.3)', padding: 5 }]} placeholder={'Eg: 75600'} keyboardType={'phone-pad'} />
-                                        </View>
+                                        <CustomTextInput
+                                            label={`Company Name`}
+                                            value={companyName}
+                                            handleChange={FormikProps.handleChange(`companyName`)}
+                                            handleBlur={FormikProps.handleBlur('companyName')}
+                                            placeholder={'Eg: Syarikat Maju Budi Sdn Bhd'}
+                                        />
+                                        <CustomTextInput
+                                            label={`Registration Number`}
+                                            value={companyRegNum}
+                                            handleChange={FormikProps.handleChange(`companyRegNum`)}
+                                            handleBlur={FormikProps.handleBlur('companyRegNum')}
+                                            placeholder={'Eg: 543-A123'}
+                                        />
+                                        <CustomTextInput
+                                            label={`Company Addresss`}
+                                            value={companyAddress}
+                                            handleChange={FormikProps.handleChange(`companyAddress`)}
+                                            handleBlur={FormikProps.handleBlur('companyAddress')}
+                                            placeholder={'Eg: 86, Jalan Budiman, Kuala Langat, Selangor'}
+                                        />
+                                        <CustomTextInput
+                                            label={`Postcode`}
+                                            value={companyPostcode}
+                                            handleChange={FormikProps.handleChange(`companyPostcode`)}
+                                            handleBlur={FormikProps.handleBlur('companyPostcode')}
+                                            placeholder={'Eg: 75600'}
+                                            keyboardType={'phone-pad'}
+                                        />
                                         <View style={[styles.formElement]}>
                                             <Text style={[styles.titleBox, { marginBottom: 10 }]}>Email</Text>
-                                            <TextInput editable={false} value={email} style={[styles.textInput,{ borderWidth: 1, borderColor: 'rgba(0,0,0,0.3)', padding: 5 }]} />
+                                            <TextInput editable={false} value={email} style={[styles.textInput, { borderWidth: 1, borderColor: 'rgba(0,0,0,0.3)', padding: 5 }]} />
                                         </View>
                                         <View style={[styles.formElement]}>
                                             <Text style={[styles.titleBox, { marginBottom: 10 }]}>Contact Number</Text>
-                                            <TextInput editable={false} value={contactNo} style={[styles.textInput,{ borderWidth: 1, borderColor: 'rgba(0,0,0,0.3)', padding: 5 }]} />
+                                            <TextInput editable={false} value={contactNo} style={[styles.textInput, { borderWidth: 1, borderColor: 'rgba(0,0,0,0.3)', padding: 5 }]} />
                                         </View>
-                                        <View style={[styles.formElement]}>
-                                            <Text style={[styles.titleBox, { marginBottom: 10 }]}>Company Registered Date</Text>
-                                            <TextInput value={companyRegisteredDate} onChangeText={FormikProps.handleChange('companyRegisteredDate')} style={[styles.textInput,{ borderWidth: 1, borderColor: 'rgba(0,0,0,0.3)', padding: 5 }]} />
-                                        </View>
-                                </ScrollView>
-                            </View>
-                            <View style={{ flexDirection: 'row', alignSelf: 'stretch' }}>
-                                <TouchableOpacity onPress={() => props.navigation.goBack()} style={{ flex: 1, borderColor: '#D3D3D3', borderWidth: 1, paddingTop: 20, paddingBottom: 20, justifyContent: 'center', alignItems: 'center' }}>
-                                    <Text style={[styles.butang, { color: '#000000' }]}>Back</Text>
-                                </TouchableOpacity>
-                                <TouchableOpacity disabled={!FormikProps.isValid} onPress={FormikProps.handleSubmit} style={{ flex: 1 }}>
-                                    <LinearGradient colors={FormikProps.isValid ? ['#0A6496', '#055E7C'] : ['rgba(10,100,150,0.5)', 'rgba(5,94,124,0.5)']} style={{ flex: 1, padding: 10, justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}>
-                                        <Text style={[styles.butang, { color: '#fff' }]}>Submit</Text>
-                                       
-                                    </LinearGradient>
-                                </TouchableOpacity>
-                            </View>
+                                        <CustomTextInput
+                                            label={`Company Registered Date`}
+                                            value={companyRegisteredDate}
+                                            handleChange={FormikProps.handleChange(`companyRegisteredDate`)}
+                                            handleBlur={FormikProps.handleBlur('companyRegisteredDate')}
+                                        />
+                                    </ScrollView>
+                                </View>
+                                <CustomFormAction
+                                navigation={props.navigation}
+                                isValid={FormikProps.isValid}
+                                handleSubmit={FormikProps.handleSubmit}
+                                label={`Submit`}
+                            />
+                            </LayoutA>
                         </View>
-                    </View>
-                </KeyboardAvoidingView>)
+                    </KeyboardAvoidingView>)
             }}
         </Formik >
     );
