f1e8a9b55459efbb7aac261d8ff351154922545f
Hafizh <eva_01@yahoo.com>
2020-03-02 17:53:08 +0800

    add layout A to New Invoice Review Screen

--- src/screens/NewInvoiceReviewScreen.js
+++ src/screens/NewInvoiceReviewScreen.js
@@ -6,6 +6,7 @@ import { LinearGradient } from 'expo-linear-gradient'
 import { Ionicons } from '@expo/vector-icons';
 import styles from '../styles/styles'
 import moment from 'moment'
+import LayoutA from '../Layout/LayoutA';
 
 const NewInvoiceReviewScreen = (props) => {
 
@@ -23,22 +24,12 @@ const NewInvoiceReviewScreen = (props) => {
     return (
 
         <KeyboardAvoidingView behavior="padding" enabled style={{ flex: 1, }} keyboardVerticalOffset={20}>
-            <View style={[styles.titleMargin, { flex: 1, flexDirection: 'row', borderBottomWidth: 1, borderColor: '#9ADAF4' }]}>
-                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start' }}>
-                    <TouchableOpacity onPress={() => props.navigation.goBack()} hitslop={{ top: 20, left: 20, bottom: 20, right: 20 }}>
-                        <Ionicons name="ios-arrow-back" color={'#3EC2D9'} style={{ fontSize: 30 }} />
-                    </TouchableOpacity>
-                </View>
-                <View style={{ flex: 5, justifyContent: 'center', alignItems: 'center' }}>
-                    <Text style={[styles.title]}>NEW INVOICE</Text>
-                </View>
-                <TouchableOpacity onPress={() => props.navigation.navigate('EditProfile')} style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-end' }}>
-                    <View style={{ backgroundColor:'rgba(62,194,217,0.5)',borderColor: "#3EC2D9", borderWidth: 0, width: 36, height: 36, borderRadius: 18, justifyContent: 'center', alignItems: 'center' }}>
-            <Ionicons name="md-person" color={'#fff'} style={{ fontSize: 25 }} />
-          </View>
-                </TouchableOpacity>
-            </View>
-            <View style={{ justifyContent: 'space-between', flex: 9 }}>
+            <LayoutA
+                title={'NEW INVOICE'}
+                screenType='form'
+                navigation={props.navigation}
+                nopadding
+            >
                 <View style={{ flex: 9 }}>
                     <ScrollView style={[styles.screenMargin]}>
                         <View style={{ margin: 5 }} />
@@ -142,7 +133,7 @@ const NewInvoiceReviewScreen = (props) => {
                         </LinearGradient>
                     </TouchableOpacity>
                 </View>
-            </View>
+            </LayoutA>
         </KeyboardAvoidingView>)
 }
 
