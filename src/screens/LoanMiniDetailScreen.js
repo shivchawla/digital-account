ca5d1eceea50e2864fcb3a2a1fef361d0c083ffe
Hafizh <eva_01@yahoo.com>
2020-03-02 17:52:45 +0800

    add layout A to Loan Mini Detail Screen

--- src/screens/LoanMiniDetailScreen.js
+++ src/screens/LoanMiniDetailScreen.js
@@ -5,6 +5,7 @@ import * as actionCreator from '../store/actions/action'
 import { Ionicons } from '@expo/vector-icons';
 import styles from '../styles/styles'
 import { ScrollView } from 'react-native-gesture-handler';
+import LayoutA from '../Layout/LayoutA';
 
 const LoanMiniDetailScreen = (props) => {
     // const loanData = useSelector(state => state.loanApplicationReducer, shallowEqual)
@@ -36,22 +37,13 @@ const LoanMiniDetailScreen = (props) => {
 
     return (
         <KeyboardAvoidingView behavior="padding" enabled style={{ flex: 1 }}>
-            <View style={{ flex: 1, flexDirection: 'row', borderBottomWidth: 1, borderColor: '#9ADAF4' }}>
-                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start', marginLeft: 0 }}>
-                    <TouchableOpacity onPress={() => props.navigation.goBack()} hitslop={{ top: 20, left: 20, bottom: 20, right: 20 }}>
-                        <Ionicons name="ios-arrow-back" color={'#3EC2D9'} style={{ fontSize: 30, paddingLeft: 20 }} />
-                    </TouchableOpacity>
-                </View>
-                <View style={{ flex: 3, justifyContent: 'center', alignItems: 'center' }}>
-                    <Text style={styles.title}>LOAN DETAIL</Text>
-                </View>
-                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-end', marginRight: 10 }}>
-                    <View style={{ backgroundColor: 'rgba(62,194,217,0.5)', borderColor: "#3EC2D9", borderWidth: 0, width: 36, height: 36, borderRadius: 18, justifyContent: 'center', alignItems: 'center' }}>
-                        <Ionicons name="md-person" color={'#fff'} style={{ fontSize: 25 }} />
-                    </View>
-                </View>
-            </View>
-            <View style={{ justifyContent: 'space-between', flex: 9 }}>
+            <LayoutA
+                title={'LOAN DETAIL'}
+                screenType='form'
+                navigation={props.navigation}
+                nopadding
+            >
+           
                 <View style={{ flex: 9 }}>
                     <ScrollView style={[styles.screenMargin, { paddingLeft: 0, paddingRight: 0 }]}>
                         {loanData && <View style={[styles.box, { marginTop: 20 }]}>
@@ -112,7 +104,7 @@ const LoanMiniDetailScreen = (props) => {
                         </View>}
                     </ScrollView>
                 </View>
-            </View>
+            </LayoutA>
         </KeyboardAvoidingView>)
 }
 
