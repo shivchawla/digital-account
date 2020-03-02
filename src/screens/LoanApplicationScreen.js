2b95bc01b4bfdb184f0d325f7d33dcc41d78020f
Hafizh <eva_01@yahoo.com>
2020-03-02 17:22:56 +0800

    add layout A,custom text input and form action to loan application

--- src/screens/LoanApplicationScreen.js
+++ src/screens/LoanApplicationScreen.js
@@ -1,4 +1,4 @@
-import React,{useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, TouchableOpacity, Text, Image, ActivityIndicator, TextInput, KeyboardAvoidingView, ScrollView, CheckBox, Platform } from 'react-native';
 import CheckBox2 from 'react-native-check-box'
 import { useDispatch } from 'react-redux'
@@ -9,6 +9,8 @@ import { Formik } from 'formik';
 import * as Yup from 'yup';
 import Layout from '../constants/Layout';
 import { keyboardBeingDisplay, keyboardBeingClose } from '../components/handleKeyboard'
+import { CustomFormAction, CustomTextInput } from '../components/Custom'
+import LayoutA from '../Layout/LayoutA';
 
 const validationSchema = Yup.object().shape({
 
@@ -60,26 +62,17 @@ const LoanApplicationScreen = (props) => {
                 return (
 
                     <KeyboardAvoidingView behavior="padding" enabled style={{ flex: 1 }} keyboardVerticalOffset={offSet ? 30 : 0}>
-                        <View style={[styles.titleMargin, { flex: 1, flexDirection: 'row', borderBottomWidth: 1, borderColor: '#9ADAF4' }]}>
-                            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start' }}>
-                                <TouchableOpacity onPress={() => props.navigation.goBack()} hitslop={{ top: 20, left: 20, bottom: 20, right: 20 }}>
-                                    <Ionicons name="ios-arrow-back" color={'#3EC2D9'} style={{ fontSize: 30 }} />
-                                </TouchableOpacity>
-                            </View>
-                            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                                <Text style={styles.title}>LOAN</Text>
-                            </View>
-                            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-end', marginRight: 10 }}>
-                                <View style={{ backgroundColor:'rgba(62,194,217,0.5)',borderColor: "#3EC2D9", borderWidth: 0, width: 36, height: 36, borderRadius: 18, justifyContent: 'center', alignItems: 'center' }}>
-            <Ionicons name="md-person" color={'#fff'} style={{ fontSize: 25 }} />
-          </View>
-                            </View>
-                        </View>
-                        <View style={{ justifyContent: 'space-between', flex: 9 }}>
+                        <LayoutA
+                            title={'LOAN'}
+                            screenType='form'
+                            navigation={props.navigation}
+                            nopadding
+                        >
+
                             <View style={{ flex: 9, margin: 10 }}>
                                 <ScrollView style={[styles.screenMargin]}>
                                     <View style={{ flexDirection: 'row', alignSelf: 'stretch', justifyContent: 'center', marginBottom: 5 }}>
-                                        <Text style={[styles.titleBox, { marginBottom: 10, marginTop: 10 , fontSize:16 }]}>Financing</Text>
+                                        <Text style={[styles.titleBox, { marginBottom: 10, marginTop: 10, fontSize: 16 }]}>Financing</Text>
                                     </View>
                                     <View style={{ flexDirection: 'row', alignSelf: 'stretch', justifyContent: 'center', marginBottom: 20 }}>
                                         <View style={{ flexDirection: 'row', width: Layout.window.width / 2, justifyContent: 'space-between' }}>
@@ -103,17 +96,29 @@ const LoanApplicationScreen = (props) => {
                                     <View style={{ flexDirection: 'row', marginBottom: 20 }}>
                                         <Text style={styles.h2}>Application Form for Financing</Text>
                                     </View>
-                                    <View style={{ marginBottom: 10 }}>
-                                        <Text style={[styles.titleBox, { marginBottom: 10,fontSize:16 }]}>Total Financing (MYR)</Text>
-                                        <TextInput value={amount} onChangeText={FormikProps.handleChange('amount')} onBlur={FormikProps.handleBlur('amount')} style={[styles.textInput,{ borderWidth: 1, borderColor: amountTouched && amountError ? 'rgba(255,0,0,1)' : 'rgba(0,0,0,0.3)', padding: 5 }]} placeholder={amountTouched && amountError ? '' : 'Eg: RM 1000.00'} placeholderTextColor={amountTouched && amountError ? 'rgba(255,0,0,0.3)' : 'lightgrey'} keyboardType={'decimal-pad'} />
-                                        {amountTouched && amountError && <Text style={styles.error}>{amountError}</Text>}
-                                    </View>
-                                    <View style={{ marginBottom: 10 }}>
-                                        <Text style={[styles.titleBox, { marginBottom: 10,fontSize:16}]}>Purpose</Text>
-                                        <TextInput value={purpose} onChangeText={FormikProps.handleChange('purpose')} onBlur={FormikProps.handleBlur('purpose')} style={[styles.textInput,{ borderWidth: 1, borderColor: purposeTouched && purposeError ? 'rgba(255,0,0,1)' : 'rgba(0,0,0,0.3)', padding: 5 }]} placeholder={purposeTouched && purposeError ? '' : 'Eg: Pay salary'} placeholderTextColor={purposeTouched && purposeError ? 'rgba(255,0,0,0.3)' : 'lightgrey'} />
+                                    <CustomTextInput
+                                        label={`Total Financing (MYR)`}
+                                        value={amount}
+                                        handleChange={FormikProps.handleChange(`amount`)}
+                                        handleBlur={FormikProps.handleBlur('amount')}
+                                        touched={amountTouched}
+                                        error={amountError}
+                                        placeholder={'Eg: RM 1000.00'}
+                                        keyboardType={'decimal-pad'} 
 
-                                        {purposeTouched && purposeError && <Text style={styles.error}>{purposeError}</Text>}
-                                    </View>
+                                    />
+                                    <CustomTextInput
+                                        label={`Purpose`}
+                                        value={purpose}
+                                        handleChange={FormikProps.handleChange(`purpose`)}
+                                        handleBlur={FormikProps.handleBlur('purpose')}
+                                        touched={purposeTouched}
+                                        error={purposeError}
+                                        placeholder={'Eg: Pay salary'}
+                                        
+
+                                    />
+                                  
                                     <View style={{ marginBottom: 20, marginTop: 25 }}>
                                         <Text style={styles.h2}>Financing Declaration</Text>
                                     </View>
@@ -137,19 +142,14 @@ const LoanApplicationScreen = (props) => {
                                     </View>
                                 </ScrollView>
                             </View>
-                            <View style={{ flexDirection: 'row', alignSelf: 'stretch' }}>
-                                <TouchableOpacity onPress={() => props.navigation.goBack()} style={{ flex: 1, borderColor: '#D3D3D3', borderWidth: 1, paddingTop: 20, paddingBottom: 20, justifyContent: 'center', alignItems: 'center' }}>
-                                        
-                                            <Text style={[styles.butang, { color: '#000000' }]}>Back</Text>
-                                        
-                                    </TouchableOpacity>
-                                <TouchableOpacity disabled={!FormikProps.isValid} onPress={FormikProps.handleSubmit} style={{ flex: 1 }}>
-                                    <LinearGradient colors={FormikProps.isValid ? ['#0A6496', '#055E7C'] : ['rgba(10,100,150,0.5)', 'rgba(5,94,124,0.5)']} style={{ flex: 1, padding: 10, justifyContent: 'center', alignItems: 'center' }}>
-                                        {FormikProps.isSubmitting ? <ActivityIndicator color={'#fff'} /> : <Text style={[styles.butang, { color: '#fff' }]}>Next</Text>}
-                                    </LinearGradient>
-                                </TouchableOpacity>
-                            </View>
-                        </View>
+                            <CustomFormAction
+                                navigation={props.navigation}
+                                isValid={FormikProps.isValid}
+                                handleSubmit={FormikProps.handleSubmit}
+                                isSubmitting={FormikProps.isSubmitting}
+                                label={`Next`}
+                            />
+                     </LayoutA>
                     </KeyboardAvoidingView>
                 )
             }}
