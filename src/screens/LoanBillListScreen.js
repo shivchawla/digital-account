6c3a2efb4f9474a9e6da39a822e793d098a8656e
Hafizh <eva_01@yahoo.com>
2020-03-02 17:23:53 +0800

    add layout A to Loan Bill List

--- src/screens/LoanBillListScreen.js
+++ src/screens/LoanBillListScreen.js
@@ -6,10 +6,11 @@ import { Ionicons } from '@expo/vector-icons';
 import moment from 'moment'
 import styles from '../styles/styles'
 import _ from 'lodash'
+import LayoutA from '../Layout/LayoutA';
 
 const LoanBillListScreen = (props) => {
     useEffect(() => {
-        const loanNo = props.route.params?.loanNo??'NA'
+        const loanNo = props.route.params?.loanNo ?? 'NA'
         console.log(`ini loan no : ${loanNo}`)
         dispatch(actionCreator.getLoanBillList(loanNo))
         //dispatch(actionCreator.getRepaymentList())
@@ -40,31 +41,23 @@ const LoanBillListScreen = (props) => {
 
     return (
         <View style={{ flex: 1, }}>
-            <View style={[styles.titleMargin, { flex: 1, flexDirection: 'row', borderBottomWidth: 1, borderColor: '#9ADAF4' }]}>
-                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start' }}>
-                    <TouchableOpacity onPress={() => props.navigation.goBack()} hitslop={{ top: 20, left: 20, bottom: 20, right: 20 }}>
-                        <Ionicons name="ios-arrow-back" color={'#3EC2D9'} style={{ fontSize: 30 }} />
-                    </TouchableOpacity>
-                </View>
-                <View style={{ flex: 5, justifyContent: 'center', alignItems: 'center' }}>
-                    <Text style={[styles.title]}>BILLS</Text>
-                </View>
-                <TouchableOpacity onPress={() => props.navigation.navigate('EditProfile')} style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-end' }}>
-                    <View style={{ backgroundColor:'rgba(62,194,217,0.5)',borderColor: "#3EC2D9", borderWidth: 0, width: 36, height: 36, borderRadius: 18, justifyContent: 'center', alignItems: 'center' }}>
-            <Ionicons name="md-person" color={'#fff'} style={{ fontSize: 25 }} />
-          </View>
-                </TouchableOpacity>
-            </View>
+           
+            <LayoutA
+                title={'BILLS'}
+                screenType='form'
+                navigation={props.navigation}
+                nopadding
+            >
             <View style={[{ flex: 9 }]}>
                 <ScrollView style={[styles.screenMargin]}>
-                   
+
                     <View style={{ marginTop: 20 }}>
                         <View style={{ flexDirection: 'row', justifyContent: 'flex-end', paddingRight: 10 }}>
                             <View style={{ flexDirection: 'row', alignItems: 'center', marginLeft: 10, flex: 1, borderWidth: 1, borderColor: 'lightgrey', padding: 10, borderRadius: 10 }}>
                                 <View>
                                     <Ionicons name="ios-search" color={'#055E7C'} style={{ fontSize: 27, paddingRight: 5 }} />
                                 </View>
-                                 <TextInput placeholder='Please Enter Keyword' style={[styles.searchBar, { flex: 4 }]}  onChangeText={(val) => searchList(val)} />
+                                <TextInput placeholder='Please Enter Keyword' style={[styles.searchBar, { flex: 4 }]} onChangeText={(val) => searchList(val)} />
                                 <TouchableOpacity onPress={props.navigation.openDrawer} >
                                     <Ionicons name="ios-options" color={'#055E7C'} style={{ fontSize: 27, paddingRight: 5 }} />
                                 </TouchableOpacity>
@@ -98,6 +91,7 @@ const LoanBillListScreen = (props) => {
                     </View>
                 </ScrollView>
             </View >
+            </LayoutA>
         </View >
 
     );
