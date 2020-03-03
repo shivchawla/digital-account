63144fca71349b6b97c9ad72d7b26a3989a12ede
Hafizh <eva_01@yahoo.com>
2020-03-03 09:41:18 +0800

    remove code===200 to code only at invoice success

--- src/screens/InvoiceSuccessScreen.js
+++ src/screens/InvoiceSuccessScreen.js
@@ -19,7 +19,7 @@ const InvoiceSuccessScreen = (props) => {
 
     return (
         <View style={{ flex: 1, paddingTop: Constants.statusBarHeight }}>
-            {code ?status ? <View style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0, }}>
+            {code ? status ? <View style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0, }}>
                 <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                 <View style={{ flex: 1, justifyContent: 'flex-end' }}>
                         <Image source={require('../assets/images/logo.png')} style={{ height: Layout.window.height * 0.2, width: Layout.window.width * 0.5 }} resizeMode={'contain'} />
