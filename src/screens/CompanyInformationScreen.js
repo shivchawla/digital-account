edd4659ff8c3f3d7ca262d4e36174f62cd43e976
Syahrizan Ali Hassan <syahrizan@fintechcapital.co>
2020-06-15 15:02:41 +0800

    updated datepicker and removed obsolete one

--- src/screens/CompanyInformationScreen.js
+++ src/screens/CompanyInformationScreen.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Image, Text, TouchableOpacity, View, TextInput, KeyboardAvoidingView, ActivityIndicator, DatePickerAndroid, DatePickerIOS, Platform, Modal } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient'
+import DateTimePicker from '@react-native-community/datetimepicker';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
 import styles from '../styles/styles'
@@ -37,6 +38,18 @@ const CompanyInformationScreen = (props) => {
     const [iosDatePickerShow, setIosDatePickerShow] = useState(false)
     const [chosenDate, setChosenDate] = useState(new Date())
 
+
+
+    const ios = Platform.OS === "ios" ? true : false
+
+    const [date, setDate] = useState(new Date(1598051730000));
+    const [mode, setMode] = useState('date');
+    const [show, setShow] = useState(false);
+
+
+
+
+
     const dispatch = useDispatch()
     const companyInfo = (values) => {
         dispatch(actionCreator.companyInfo(values))
@@ -62,26 +75,26 @@ const CompanyInformationScreen = (props) => {
             validationSchema={validationSchema}
         >
             {FormikProps => {
-                const datePicker = async () => {
-                    if (Platform.OS === 'android') {
-                        try {
-                            const { action, year, month, day } = await DatePickerAndroid.open({
-                                // Use `new Date()` for current date.
-                                // May 25 2020. Month 0 is January.
-                                date: new Date(2020, 4, 25),
-                            });
-                            if (action !== DatePickerAndroid.dismissedAction) {
-                                // Selected year, month (0-11), day
-                                FormikProps.setFieldValue('cddRegisteredDate', `${year}-${month}-${day}`)
-                            }
-                        } catch ({ code, message }) {
-                            console.warn('Cannot open date picker', message);
-                        }
-                    } else {
-                        setIosDatePickerShow(true)
-                    }
 
-                }
+
+
+                const onChange = (event, selectedDate) => {
+                    const currentDate = selectedDate || date;
+                    //console.log(`selected date ialah ${moment(currentDate).format('Y-M-D')}`)
+                    setShow(ios);
+                    setDate(currentDate);
+                    FormikProps.setFieldValue('cddRegisteredDate', moment(currentDate).format('Y-M-D'))
+                };
+
+                const showMode = currentMode => {
+                    setShow(true);
+                    setMode(currentMode);
+                };
+
+                const showDatepicker = () => {
+                    showMode('date');
+                };
+
 
                 const { cddCompanyName, cddRegistrationNumber, cddRegisteredDate } = FormikProps.values
 
@@ -96,24 +109,23 @@ const CompanyInformationScreen = (props) => {
 
                 return (
 
-                        
-                        <LayoutA
-                            title={'COMPANY INFO'}
-                            screenType='registration'
-                            navigation={props.navigation}
-                            nopadding
-                        >
-                            <Modal
-                            animationType="slide"
-                            transparent={false}
-                            visible={iosDatePickerShow}
-                            onRequestClose={() => {
+
+                    <LayoutA
+                        title={'COMPANY INFO'}
+                        screenType='registration'
+                        navigation={props.navigation}
+                        nopadding
+                    >
+                        <Modal animationType={'slide'}
+                            visible={ios && show} onRequestClose={() => {
                                 console.log(`test`)
+                                setIosDatePickerShow(!iosDatePickerShow)
+
                             }}>
-                            <View style={{ flex: 1, paddingTop: Constants.statusBarHeight }}>
+                            <View style={styles.container}>
                                 <View style={[styles.titleMargin, { flex: 1, flexDirection: 'row', borderBottomWidth: 1, borderColor: '#9ADAF4', marginBottom: 25 }]}>
                                     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start', marginLeft: 0 }}>
-                                        <TouchableOpacity onPress={() => setIosDatePickerShow(!iosDatePickerShow)} hitslop={{ top: 20, left: 20, bottom: 20, right: 20 }}>
+                                        <TouchableOpacity onPress={() => setShow(!ios)} hitslop={{ top: 20, left: 20, bottom: 20, right: 20 }}>
                                             <Ionicons name="ios-arrow-back" color={'#3EC2D9'} style={{ fontSize: 30, paddingLeft: 20 }} />
                                         </TouchableOpacity>
                                     </View>
@@ -123,60 +135,73 @@ const CompanyInformationScreen = (props) => {
                                 </View>
                                 <View style={{ flex: 9, justifyContent: 'flex-start' }}>
 
-                                    <DatePickerIOS
-                                        mode={'date'}
-                                        date={chosenDate}
-                                        //onDateChange={()FormikProps.setFieldValue('cddRegisteredDate', `${year}-${month}-${day}`)}
-                                        onDateChange={(val) => {
-                                            FormikProps.setFieldValue('cddRegisteredDate', moment(val).format(`Y-M-D`))
-                                            setChosenDate(val)
-                                        }}
+                                    <DateTimePicker
+                                        testID="dateTimePicker"
+                                        //timeZoneOffsetInMinutes={0}
+                                        value={date}
+                                        mode={mode}
+                                        is24Hour={true}
+                                        display="default"
+                                        onChange={onChange}
                                     />
                                 </View>
                             </View>
                         </Modal>
-                            <View style={[styles.screenMargin, { flex: 9 }]}>
-                                <CustomTextInput
-                                    label={`Company Name`}
-                                    value={cddCompanyName}
-                                    handleChange={FormikProps.handleChange(`cddCompanyName`)}
-                                    handleBlur={FormikProps.handleBlur(`cddCompanyName`)}
-                                    touched={cddCompanyNameTouched}
-                                    error={cddCompanyNameError}
-                                    placeholder={'Eg: Syarikat ABC Sdn Bhd'}
+                        <View style={[styles.screenMargin, { flex: 9 }]}>
+                            <CustomTextInput
+                                label={`Company Name`}
+                                value={cddCompanyName}
+                                handleChange={FormikProps.handleChange(`cddCompanyName`)}
+                                handleBlur={FormikProps.handleBlur(`cddCompanyName`)}
+                                touched={cddCompanyNameTouched}
+                                error={cddCompanyNameError}
+                                placeholder={'Eg: Syarikat ABC Sdn Bhd'}
 
-                                />
-                                <CustomTextInput
-                                    label={`Registration Number`}
-                                    value={cddRegistrationNumber}
-                                    handleChange={FormikProps.handleChange(`cddRegistrationNumber`)}
-                                    handleBlur={FormikProps.handleBlur(`cddRegistrationNumber`)}
-                                    touched={cddRegistrationNumberTouched}
-                                    error={cddRegistrationNumberError}
-                                    placeholder={'Eg: 105015-A'}
+                            />
+                            <CustomTextInput
+                                label={`Registration Number`}
+                                value={cddRegistrationNumber}
+                                handleChange={FormikProps.handleChange(`cddRegistrationNumber`)}
+                                handleBlur={FormikProps.handleBlur(`cddRegistrationNumber`)}
+                                touched={cddRegistrationNumberTouched}
+                                error={cddRegistrationNumberError}
+                                placeholder={'Eg: 105015-A'}
 
-                                />
-                                <View style={[styles.formElement]}>
-                                    <Text style={[styles.titleBox], { fontSize: 15 }}>Registration Date</Text>
-                                    <View style={{ flexDirection: 'row' }}>
-                                        <TouchableOpacity onPress={datePicker}>
-                                            <Image source={require('../assets/images/calendar.png')} style={{ width: 30, height: 30, marginRight: 10 }} resizeMode={'contain'} />
-                                        </TouchableOpacity>
-                                        <TextInput editable={false} style={{ flex: 1, alignSelf: 'center' }} value={cddRegisteredDate} style={[styles.textInput, { flex: 1, borderWidth: 1, borderColor: 'rgba(0,0,0,0.3)', padding: 5 }]} />
-                                    </View>
-                                    {cddRegisteredDateTouched && cddRegisteredDateError && <Text style={styles.error}>{cddRegisteredDateError}</Text>}
-                                </View>
-                            </View>
-                            <CustomFormAction
-                                navigation={props.navigation}
-                                isValid={FormikProps.isValid}
-                                handleSubmit={FormikProps.handleSubmit}
-                                isSubmitting = {FormikProps.isSubmitting}
-                                label={`Next`}
                             />
 
-                        </LayoutA>
-                    )
+                            <View style={[styles.formElement]}>
+                                <Text style={[styles.titleBox], { fontSize: 15 }}>Registration </Text>
+                                <View style={{ flexDirection: 'row' }}>
+                                    <TouchableOpacity onPress={() => showDatepicker()}>
+                                        <Image source={require('../assets/images/calendar.png')} style={{ width: 30, height: 30, marginRight: 10 }} resizeMode={'contain'} />
+                                    </TouchableOpacity>
+                                    <TextInput editable={false} style={{ flex: 1, alignSelf: 'center' }} value={cddRegisteredDate} style={[styles.textInput, { flex: 1, borderWidth: 1, borderColor: 'rgba(0,0,0,0.3)', padding: 5 }]} />
+                                </View>
+                                {cddRegisteredDateTouched && cddRegisteredDateError && <Text style={styles.error}>{cddRegisteredDateError}</Text>}
+                            </View>
+                            {!ios && show && (
+                                <DateTimePicker
+                                    testID="dateTimePicker"
+                                    timeZoneOffsetInMinutes={0}
+                                    value={date}
+                                    mode={mode}
+                                    is24Hour={true}
+                                    display="default"
+                                    onChange={onChange}
+                                />
+                            )}
+
+                        </View>
+                        <CustomFormAction
+                            navigation={props.navigation}
+                            isValid={FormikProps.isValid}
+                            handleSubmit={FormikProps.handleSubmit}
+                            isSubmitting={FormikProps.isSubmitting}
+                            label={`Next`}
+                        />
+
+                    </LayoutA>
+                )
             }}
         </Formik >
 
